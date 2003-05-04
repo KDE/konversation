aff37e3703ad8de9a10225d52494255802bd49bd
Dario Abatianni <eisfuchs@tigress.com>
2003-05-04 21:00:26 +0000

    Channel list work (hopefullycd '/attic/backup/home/eisfuchs/Development/cvs/eisfuchs/konversation') done
    svn path=/trunk/kdeextragear-2/konversation/; revision=223894

--- konversation/channellistpanel.cpp
+++ konversation/channellistpanel.cpp
@@ -18,12 +18,13 @@
 #include <qgrid.h>
 #include <qlabel.h>
 #include <qspinbox.h>
-#include <qcheckbox.h>
 #include <qpushbutton.h>
 #include <qhgroupbox.h>
+#include <qregexp.h>
 
 #include <klineedit.h>
 #include <klocale.h>
+#include <kdebug.h>
 
 #include "channellistpanel.h"
 #include "server.h"
@@ -36,6 +37,15 @@ ChannelListPanel::ChannelListPanel(QWidget* parent) :
   setNumChannels(0);
   setNumUsers(0);
 
+  setMinUsers(0);
+  setMaxUsers(0);
+
+  setChannelTarget(true);
+  setTopicTarget(false);
+  setRegExp(false);
+
+  filterTextChanged(QString::null);
+
   QHGroupBox* filterGroup=new QHGroupBox(i18n("Filter settings"),this);
   QGrid* mainGrid=new QGrid(2,Qt::Vertical,filterGroup);
   mainGrid->setSpacing(spacing());
@@ -44,34 +54,56 @@ ChannelListPanel::ChannelListPanel(QWidget* parent) :
   new QLabel(i18n("Maximum users:"),mainGrid);
   QSpinBox* minUsersSpin=new QSpinBox(mainGrid,"min_users_spin");
   QSpinBox* maxUsersSpin=new QSpinBox(mainGrid,"max_users_spin");
-  
+  minUsersSpin->setValue(getMinUsers());
+  maxUsersSpin->setValue(getMaxUsers());
+
   new QLabel(i18n("Filter pattern:"),mainGrid);
   new QLabel(i18n("Filter target:"),mainGrid);
-  
+
   KLineEdit* filterInput=new KLineEdit(mainGrid,"channel_list_filter_input");
+  filterInput->setText(getFilterText());
 
   QHBox* targetBox=new QHBox(mainGrid);
   targetBox->setSpacing(spacing());
-  
-  QCheckBox* channelFilter=new QCheckBox(i18n("Channel"),targetBox,"filter_target_channel_check");  
-  QCheckBox* topicFilter=new QCheckBox(i18n("Topic"),targetBox,"filter_target_topic_check");
-  QCheckBox* regexCheck=new QCheckBox(i18n("Regular expression"),targetBox,"regex_check");
-  QPushButton* applyFilter=new QPushButton(i18n("Apply filter"),targetBox,"apply_filter_button");     
-  
+
+  channelFilter=new QCheckBox(i18n("Channel"),targetBox,"filter_target_channel_check");
+  topicFilter=new QCheckBox(i18n("Topic"),targetBox,"filter_target_topic_check");
+  regexpCheck=new QCheckBox(i18n("Regular expression"),targetBox,"regexp_check");
+  QPushButton* applyFilter=new QPushButton(i18n("Apply filter"),targetBox,"apply_filter_button");
+
+  channelFilter->setChecked(getChannelTarget());
+  topicFilter->setChecked(getTopicTarget());
+  regexpCheck->setChecked(getRegExp());
+
   targetBox->setStretchFactor(topicFilter,10);
-  
+
   channelListView=new KListView(this,"channel_list_view");
   channelListView->addColumn(i18n("Channel name"));
   channelListView->addColumn(i18n("Users"));
   channelListView->addColumn(i18n("Channel topic"));
   channelListView->setFullWidth(true);
+  channelListView->setAllColumnsShowFocus(true);
 
   QHBox* actionBox=new QHBox(this);
   actionBox->setSpacing(spacing());
-  
+
   QPushButton* refreshListButton=new QPushButton(i18n("Refresh list"),actionBox,"refresh_list_button");
   QPushButton* saveListButton=new QPushButton(i18n("Save list"),actionBox,"save_list_button");
   QPushButton* joinChannelButton=new QPushButton(i18n("Join channel"),actionBox,"join_channel_button");
+
+  connect(minUsersSpin,SIGNAL (valueChanged(int)),this,SLOT(setMinUsers(int)) );
+  connect(maxUsersSpin,SIGNAL (valueChanged(int)),this,SLOT(setMaxUsers(int)) );
+  connect(this,SIGNAL (adjustMinValue(int)),minUsersSpin,SLOT (setValue(int)) );
+  connect(this,SIGNAL (adjustMaxValue(int)),maxUsersSpin,SLOT (setValue(int)) );
+
+  connect(filterInput,SIGNAL (textChanged(const QString&)),this,SLOT (filterTextChanged(const QString&)) );
+  connect(filterInput,SIGNAL (returnPressed()),this,SLOT (applyFilterClicked()) );
+
+  connect(channelFilter,SIGNAL (clicked()),this,SLOT (channelTargetClicked()) );
+  connect(topicFilter,SIGNAL (clicked()),this,SLOT (topicTargetClicked()) );
+  connect(regexpCheck,SIGNAL (clicked()),this,SLOT (regExpClicked()) );
+
+  connect(applyFilter,SIGNAL (clicked()),this,SLOT (applyFilterClicked()) );
 
   connect(refreshListButton,SIGNAL (clicked()),this,SLOT (refreshList()) );
   connect(saveListButton,SIGNAL (clicked()),this,SLOT (saveList()) );
@@ -85,8 +117,11 @@ ChannelListPanel::~ChannelListPanel()
 void ChannelListPanel::refreshList()
 {
   channelListView->clear();
+  channelList.clear();
+
   setNumChannels(0);
   setNumUsers(0);
+
   emit refreshChannelList();
 }
 
@@ -106,7 +141,28 @@ void ChannelListPanel::joinChannelClicked()
 void ChannelListPanel::addToChannelList(const QString& channel,int users,const QString& topic)
 {
   new KListViewItem(channelListView,channel,QString::number(users),topic);
+  channelList.append(channel+" "+QString::number(users)+" "+topic);
+
+  setNumChannels(getNumChannels()+1);
+  setNumUsers(getNumUsers()+users);
 }
+
+void ChannelListPanel::filterTextChanged(const QString& newText)
+{
+  filterText=newText;
+}
+
+int ChannelListPanel::getNumChannels() { return numChannels; }
+int ChannelListPanel::getNumUsers()    { return numUsers; }
+
+int ChannelListPanel::getMinUsers()    { return minUsers; }
+int ChannelListPanel::getMaxUsers()    { return maxUsers; }
+
+bool ChannelListPanel::getChannelTarget() { return channelTarget; }
+bool ChannelListPanel::getTopicTarget()   { return topicTarget; }
+bool ChannelListPanel::getRegExp()        { return regExp; }
+
+const QString& ChannelListPanel::getFilterText() { return filterText; }
 
 void ChannelListPanel::setNumChannels(int num)
 {
@@ -120,8 +176,68 @@ void ChannelListPanel::setNumUsers(int num)
   // update widgets here
 }
 
-int ChannelListPanel::getNumChannels() { return numChannels; }
-int ChannelListPanel::getNumUsers()    { return numUsers; }
+void ChannelListPanel::setMinUsers(int num)
+{
+  minUsers=num;
+  if(minUsers>maxUsers) adjustMaxValue(num);
+}
+
+void ChannelListPanel::setMaxUsers(int num)
+{
+  maxUsers=num;
+  if(maxUsers<minUsers) adjustMinValue(num);
+}
+
+void ChannelListPanel::setChannelTarget(bool state)  { channelTarget=state; }
+void ChannelListPanel::setTopicTarget(bool state)    { topicTarget=state; }
+
+void ChannelListPanel::setRegExp(bool state)         { regExp=state; }
+
+void ChannelListPanel::channelTargetClicked()        { setChannelTarget(channelFilter->state()==2); }
+void ChannelListPanel::topicTargetClicked()          { setTopicTarget(topicFilter->state()==2); }
+void ChannelListPanel::regExpClicked()               { setRegExp(regexpCheck->state()==2); }
+
+void ChannelListPanel::applyFilterClicked()
+{
+  unsigned int index=0;
+
+  QListViewItem* item=channelListView->itemAtIndex(0);
+
+  while(item)
+  {
+    bool visible=true;
+
+    if(getMaxUsers())
+    {
+      if(item->text(1).toInt()<getMinUsers() ||
+         item->text(1).toInt()>getMaxUsers() ) visible=false;
+    }
+
+    if(!getFilterText().isEmpty())
+    {
+      if(getChannelTarget())
+      {
+        if(item->text(0).find(QRegExp(getFilterText(),false,!getRegExp()))==-1) visible=false;
+      }
+
+      if(getTopicTarget())
+      {
+        if(item->text(2).find(QRegExp(getFilterText(),false,!getRegExp()))==-1) visible=false;
+      }
+    }
+
+    item->setVisible(visible);
+
+    item=channelListView->itemAtIndex(++index);
+  }
+}
+
+void ChannelListPanel::closeYourself()
+{
+  // make the server delete us so server can reset the pointer to us
+  kdDebug() << "ChannelListPanel::closeYourself()" << endl;
+  server->closeChannelListPanel();
+}
 
 void ChannelListPanel::adjustFocus()
 {
--- konversation/channellistpanel.h
+++ konversation/channellistpanel.h
@@ -17,6 +17,8 @@
 #ifndef _CHANNELLISTPANEL_H_
 #define _CHANNELLISTPANEL_H_
 
+#include <qcheckbox.h>
+
 #include <klistview.h>
 
 #include "chatwindow.h"
@@ -33,30 +35,71 @@ class ChannelListPanel : public ChatWindow
     ChannelListPanel(QWidget* parent);
     ~ChannelListPanel();
 
+    void closeYourself();
+
   signals:
     void refreshChannelList();
     void joinChannel(const QString& channelName);
+    void adjustMinValue(int num);
+    void adjustMaxValue(int num);
 
   public slots:
     void adjustFocus();
     void addToChannelList(const QString& channel,int users,const QString& topic);
 
   protected slots:
+    void applyFilterClicked();
     void refreshList();
     void saveList();
     void joinChannelClicked();
 
+    void setMinUsers(int num);
+    void setMaxUsers(int num);
+
+    void filterTextChanged(const QString& newText);
+    void channelTargetClicked();
+    void topicTargetClicked();
+    void regExpClicked();
+
   protected:
     void setNumChannels(int num);
-    void setNumUsers(int num);
-
     int getNumChannels();
+    void setNumUsers(int num);
     int getNumUsers();
+
+    void setChannelTarget(bool state);
+    bool getChannelTarget();
+
+    void setTopicTarget(bool state);
+    bool getTopicTarget();
+
+    void setRegExp(bool state);
+    bool getRegExp();
+
+    int getMinUsers();
+    int getMaxUsers();
+
+    const QString& getFilterText();
 
     int numChannels;
     int numUsers;
 
+    int minUsers;
+    int maxUsers;
+
+    bool channelTarget;
+    bool topicTarget;
+
+    bool regExp;
+
+    QCheckBox* channelFilter;
+    QCheckBox* topicFilter;
+    QCheckBox* regexpCheck;
+
     KListView* channelListView;
+    QStringList channelList;
+
+    QString filterText;
 };
 
 #endif
