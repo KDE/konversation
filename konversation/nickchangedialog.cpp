7263c681d7c6b7e2e501351c456797026936ca36
Dario Abatianni <eisfuchs@tigress.com>
2003-11-22 21:35:31 +0000

    Current nick does not get inserted into nick list anymore if it is already in there
    svn path=/trunk/kdeextragear-2/konversation/; revision=268935

--- konversation/nickchangedialog.cpp
+++ konversation/nickchangedialog.cpp
@@ -6,7 +6,7 @@
 */
 
 /*
-  nickchangedialog.cpp  -  description
+  nickchangedialog.cpp  -  Shows a small dialog where the user can change their nickname
   begin:     Mon Jul 8 2002
   copyright: (C) 2002 by Dario Abatianni
   email:     eisfuchs@tigress.com
@@ -26,22 +26,24 @@ NickChangeDialog::NickChangeDialog(QWidget* parent,QString currentNick,const QSt
                               KDialogBase::Ok | KDialogBase::Cancel,
                               KDialogBase::Ok,true)
 {
-  /* Create the top level widget */
+  // Create the top level widget
   QWidget* page=new QWidget(this);
   setMainWidget(page);
-  /* Add the layout to the widget */
+  // Add the layout to the widget
   QHBoxLayout* dialogLayout=new QHBoxLayout(page);
   dialogLayout->setSpacing(spacingHint());
-  /* Add the nickname input widget */
+  // Add the nickname input widget
   nicknameInput=new QComboBox(true,page,"nickname_input");
-  
+
   nicknameInput->insertStringList(nickList);
-  nicknameInput->insertItem(currentNick,0);
+  // only insert current nick if it isn't already in the list
+  if(!nickList.contains(currentNick)) nicknameInput->insertItem(currentNick,0);
+  nicknameInput->setCurrentText(currentNick);
 
   dialogLayout->addWidget(nicknameInput);
 
   connect(nicknameInput,SIGNAL (activated(const QString&)),this,SLOT (newNicknameEntered(const QString&)) );
-  
+
   setButtonOKText(i18n("&OK"),i18n("Set new nickname and close the window"));
   setButtonCancelText(i18n("&Cancel"),i18n("Close the window without changes"));
 
