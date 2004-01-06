0f5ea2280871b9ab2f1a61ca5f6ce7d678bd6578
Dario Abatianni <eisfuchs@tigress.com>
2004-01-06 12:02:39 +0000

    Fix for bug 71621 based on a suggestion by Peter Simonsson
    svn path=/trunk/kdeextragear-2/konversation/; revision=277220

--- konversation/inputfilter.cpp
+++ konversation/inputfilter.cpp
@@ -722,6 +722,8 @@ void InputFilter::parseServerCommand(const QString &prefix, const QString &comma
         {
           if(numeric==RPL_WELCOME)
           {
+            // re-set nickname, since the server may have truncated it
+            server->setNickname(parameterList[0]);
             // Remember server's insternal name
             server->setIrcName(prefix);
             // Send the welcome signal, so the server class knows we are connected properly
