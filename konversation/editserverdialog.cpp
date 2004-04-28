0cb8ab72894651005768f11e2c7f173f00f5749f
Dario Abatianni <eisfuchs@tigress.com>
2004-04-28 16:06:18 +0000

    Added WhatsThis messages done by Nathanial Turner
    svn path=/trunk/kdeextragear-2/konversation/; revision=307016

--- konversation/editserverdialog.cpp
+++ konversation/editserverdialog.cpp
@@ -15,6 +15,7 @@
 #include <qhbox.h>
 #include <qlayout.h>
 #include <qlabel.h>
+#include <qwhatsthis.h>
 
 #include <klineedit.h>
 #include <kcombobox.h>
@@ -51,7 +52,12 @@ EditServerDialog::EditServerDialog(QWidget* parent,
   groupNameLabel->setBuddy(groupNameInput);
 
   QLabel* identityLabel=new QLabel(i18n("&Identity:"),page);
+  QString identityWT = i18n("Select the identity to use with this server. "
+                            "(See the Identity page in the Preferences to "
+                            "add or modify identities.)");
+  QWhatsThis::add(identityLabel, identityWT);
   identityCombo=new KComboBox(page);
+  QWhatsThis::add(identityCombo, identityWT);
   identityLabel->setBuddy(identityCombo);
 
   QPtrList<Identity> identities=KonversationApplication::preferences.getIdentityList();
@@ -64,22 +70,44 @@ EditServerDialog::EditServerDialog(QWidget* parent,
   }
 
   QLabel* serverNameLabel=new QLabel(i18n("&Server name:"),page);
+  QString serverNameWT = i18n("Enter the host name of the IRC server here.\n\n"
+                              "Example:  irc.kde.org");
   QHBox* serverBox=new QHBox(page);
   serverBox->setSpacing(spacingHint());
   serverNameInput=new KLineEdit(name,serverBox);
+  QWhatsThis::add(serverNameInput, serverNameWT);
   serverNameLabel->setBuddy(serverNameInput);
+  QWhatsThis::add(serverNameLabel, serverNameWT);
 
   QLabel* serverPortLabel=new QLabel(i18n("P&ort:"),serverBox);
+  QString serverPortWT = i18n("Enter the port number on which the IRC "
+                              "server listens.  (The default IRC port is "
+                              "6667 and usually need not be changed.)\n\n"
+                              "Example: 6667");
+  QWhatsThis::add(serverPortLabel, serverPortWT);
   serverPortInput=new KLineEdit(port,serverBox);
+  QWhatsThis::add(serverPortInput, serverPortWT);
   serverPortLabel->setBuddy(serverPortInput);
 
   QLabel* serverKeyLabel=new QLabel(i18n("&Password:"),page);
+  QString serverKeyWT = i18n("If your IRC server requires a password, enter "
+                             "it here.  (Most servers do not require a "
+                             "password.)");
+  QWhatsThis::add(serverKeyLabel, serverKeyWT);
   serverKeyInput=new KLineEdit(serverKey,page);
   serverKeyInput->setEchoMode(QLineEdit::Password);
+  QWhatsThis::add(serverKeyInput, serverKeyWT);
   serverKeyLabel->setBuddy(serverKeyInput);
 
   QLabel* channelNameLabel=new QLabel(i18n("C&hannel name:"),page);
+  QString channelNameWT = i18n("Enter one or more channel names here.  These "
+                               "channels will be joined automatically when "
+                               "this server is connected.  (Separate multiple "
+                               "channel names with spaces.)\n\n"
+                               "Example:  #debian #kde-users");
+  QWhatsThis::add(channelNameLabel, channelNameWT);
   channelNameInput=new KLineEdit(channelName,page);
+  QWhatsThis::add(channelNameInput, channelNameWT);
   channelNameLabel->setBuddy(channelNameInput);
 
   QLabel* channelKeyLabel=new QLabel(i18n("Pass&word:"),page);
