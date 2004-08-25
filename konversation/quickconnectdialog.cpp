00f972b511fc12dccb5e935685eaedd6e5bd5fe9
Stephan Binner <binner@kde.org>
2004-08-25 12:59:53 +0000

    Fixed accel conflict
    svn path=/trunk/kdeextragear-2/konversation/; revision=340887

--- konversation/quickconnectdialog.cpp
+++ konversation/quickconnectdialog.cpp
@@ -24,55 +24,55 @@
 
 
 QuickConnectDialog::QuickConnectDialog(QWidget *parent)
-				:KDialogBase(parent, "quickconnect", true, i18n("Quick Connect"), 
+				:KDialogBase(parent, "quickconnect", true, i18n("Quick Connect"),
 							KDialogBase::Ok | KDialogBase::Cancel, KDialogBase::Ok, true)
 {
 	QWidget* page = new QWidget(this);
-	setMainWidget(page);	
-	
+	setMainWidget(page);
+
 	QGridLayout* layout = new QGridLayout(page, 2, 4);
 	layout->setSpacing(spacingHint());
 	layout->setColStretch(1, 10);
-	
+
 	QLabel* hostNameLabel = new QLabel(i18n("&Server host:"), page);
 	QString hostNameWT = i18n("Enter the host of the network here.");
 	QWhatsThis::add(hostNameLabel, hostNameWT);
 	hostNameInput = new KLineEdit(page);
 	QWhatsThis::add(hostNameInput, hostNameWT);
 	hostNameLabel->setBuddy(hostNameInput);
-	
+
 	QLabel* portLabel = new QLabel(i18n("&Port:"), page);
 	QString portWT = i18n("The port that the IRC server is using.");
 	QWhatsThis::add(portLabel, portWT);
 	portInput = new KLineEdit("6667", page );
 	QWhatsThis::add(portInput, portWT);
 	portLabel->setBuddy(portInput);
-	
+
 	QLabel* nickLabel = new QLabel(i18n("&Nick:"), page);
 	QString nickWT = i18n("The nick you want to use.");
 	QWhatsThis::add(nickLabel, nickWT);
 	nickInput = new KLineEdit(KonversationApplication::preferences.getNickname(0), page);
 	QWhatsThis::add(nickInput, nickWT);
 	nickLabel->setBuddy(nickInput);
-	
+
 	QLabel* passwordLabel = new QLabel(i18n("P&assword:"), page);
 	QString passwordWT = i18n("If your IRC server requires a password, enter it here (most servers do not require a password.)");
 	QWhatsThis::add(passwordLabel, passwordWT);
 	passwordInput = new KLineEdit(page);
 	QWhatsThis::add(passwordInput, passwordWT);
 	passwordLabel->setBuddy(passwordInput);
-	
+
 	layout->addWidget(hostNameLabel, 0, 0);
 	layout->addWidget(hostNameInput, 0, 1);
 	layout->addWidget(portLabel, 0, 2);
 	layout->addWidget(portInput, 0, 3);
-	
+
 	layout->addWidget(nickLabel, 1, 0);
 	layout->addWidget(nickInput, 1, 1);
 	layout->addWidget(passwordLabel, 1, 2);
 	layout->addWidget(passwordInput, 1, 3);
-	
-	setButtonOK(KGuiItem(i18n("&Connect"),"connect_creating",i18n("Connect to the server")));
+
+	setButtonOK(KGuiItem(i18n("C&onnect"),"connect_creating",i18n("Connect to the server")));
 }
 
 QuickConnectDialog::~QuickConnectDialog()
@@ -81,7 +81,7 @@ QuickConnectDialog::~QuickConnectDialog()
 
 void QuickConnectDialog::slotOk()
 {
-	if(!hostNameInput->text().isEmpty() && 
+	if(!hostNameInput->text().isEmpty() &&
 	    !portInput->text().isEmpty() &&
 	    !nickInput->text().isEmpty())
 	{
