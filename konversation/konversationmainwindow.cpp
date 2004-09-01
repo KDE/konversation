c3ea36b71f69baeac1c74b5c30c97a44f7dd3577
Andrew Coles <andrew_coles@yahoo.co.uk>
2004-09-01 13:00:40 +0000

    CVS_SILENT
    Corrected i18n plural-form usage.
    
    svn path=/trunk/kdeextragear-2/konversation/; revision=343211

--- konversation/konversationmainwindow.cpp
+++ konversation/konversationmainwindow.cpp
@@ -1067,32 +1067,20 @@ void KonversationMainWindow::tooLongLag(Server* lagServer,int msec)
     int secsleft = msec/1000;
     QStringList timeString;
 
-    if(secsleft%60 == 1) {
-	    timeString.push_front(i18n("1 second"));
-	    secsleft -= 1;
-    } else if(secsleft %60) {
-	    timeString.push_front(i18n("%1 seconds").arg(secsleft%60));
+    if(secsleft %60) {
+	    timeString.push_front(i18n("1 second", "%n seconds", secsleft%60));
 	    secsleft -= secsleft%60;
     }
-    if(secsleft % 360 == 60) {
-	    timeString.push_front(i18n("1 minute"));
-	    secsleft -= 60;
-    } else if(secsleft % 360) {
-	    timeString.push_front(i18n("%1 minutes").arg((secsleft%360)/60));
+    if(secsleft % 360) {
+	    timeString.push_front(i18n("1 minute", "%n minutes", (secsleft%360)/60));
 	    secsleft -= secsleft%360;
     }
-    if(secsleft % (360*24) == 360){
-	    timeString.push_front(i18n("1 hour"));
-            secsleft -= 360;
-    } else if(secsleft % (360*24)) {
-            timeString.push_front(i18n("%1 hours").arg((secsleft%360*24)/360));
+    if(secsleft % (360*24)) {
+            timeString.push_front(i18n("1 hour", "%n hours", (secsleft%360*24)/360));
             secsleft -= secsleft%(360*24);
     }
-    if(secsleft == (360*24)) {
-            timeString.push_front(i18n("1 day"));
-            secsleft -= 360*24;
-    } else if(secsleft) {
-            timeString.push_front(i18n("%1 days").arg(secsleft/(360*24)));
+    if(secsleft) {
+            timeString.push_front(i18n("1 day", "%n days", secsleft/(360*24)));
     }
     Q_ASSERT(!timeString.empty()); if(timeString.empty()) return;
     QString timestring = timeString.first();
