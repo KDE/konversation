91cd5b14354682dcddc61a39cf6fb7df26b883e2
Gary Cramblitt <garycramblitt@comcast.net>
2004-09-10 23:46:03 +0000

    Not tested yet, but it compiles.
    svn path=/trunk/kdeextragear-2/konversation/; revision=345584

--- konversation/serverison.cpp
+++ konversation/serverison.cpp
@@ -15,78 +15,174 @@
   email:     john@geola.co.uk
 */
 
-#include "server.h"
-#include "serverison.h"
-#include "addressbook.h"
-#include "konversationapplication.h"
+// Qt includes.
+#include <qmap.h>
 #include <qstring.h>
 #include <qstringlist.h>
 #include <kabc/addressbook.h>
 #include <kabc/stdaddressbook.h>
 
-ServerISON::ServerISON(const Server *const server) : m_server(server) {
-  connect( Konversation::Addressbook::self()->getAddressBook(), SIGNAL( addressBookChanged( AddressBook * ) ), this, SLOT( recalculateAddressees() ) );
-  connect( Konversation::Addressbook::self(), SIGNAL(addresseesChanged()), this, SLOT(recalculateAddressees()));
-  connect( m_server, SIGNAL(nickInfoChanged(Server*, const NickInfoPtr)), this, SLOT(nickInfoChanged(Server*, const NickInfoPtr)));
-			  
+// Konversation includes.
+#include "server.h"
+#include "serverison.h"
+#include "addressbook.h"
+#include "konversationapplication.h"
+#include "nickinfo.h"
+#include "konversationmainwindow.h"
+
+ServerISON::ServerISON(Server* server) : m_server(server) {
+  connect( Konversation::Addressbook::self()->getAddressBook(),
+    SIGNAL( addressBookChanged( AddressBook * ) ),
+    this, SLOT( recalculateAddressees() ) );
+  connect( Konversation::Addressbook::self(), SIGNAL(addresseesChanged()),
+    this, SLOT(recalculateAddressees()));
+  connect( m_server, SIGNAL(nickInfoChanged(Server*, const NickInfoPtr)),
+    this, SLOT(nickInfoChanged(Server*, const NickInfoPtr)));
+  connect(m_server->getMainWindow(),SIGNAL(prefsChanged()),
+    this,SLOT(slotPrefsChanged()));
 }
     
-QString ServerISON::getISON() {
-  //TODO unique this list?  Be better, but probably not worth it.
-  return getAddressees().join(" ") + " " + getWatchedNicks();
-  
+QStringList ServerISON::getISONList() { return m_ISONList; }
+
+QStringList ServerISON::getAddressees() { return m_addresseesISON; }
+
+KABC::Addressee ServerISON::getOfflineNickAddresse(QString& nickname)
+{
+  if (m_offlineNickToAddresseeMap.contains(nickname))
+    return m_offlineNickToAddresseeMap[nickname];
+  else
+    return KABC::Addressee();
 }
 
-QStringList ServerISON::getAddressees(){
-//TODO - remove the below line when we don't need it anymore.  It _should_ work, but leave it in while testing, to iron out bugs first
-  recalculateAddressees();
-  return m_addresseesISON;
-}
-void ServerISON::recalculateAddressees() {
-  m_addresseesISON.clear();
-  for( KABC::AddressBook::ConstIterator it = Konversation::Addressbook::self()->getAddressBook()->begin(); 
-		  it != Konversation::Addressbook::self()->getAddressBook()->end(); ++it ) {
-    if(Konversation::Addressbook::self()->hasAnyNicks(*it)) {
-      QString uid = (*it).uid();
-      bool found = false;
-      //First check if we already know that this nick is online.
-      //TODO we really should have a map :( 
-      for(NickInfoMap::ConstIterator nickInfoIt=m_server->getNicksOnline()->begin(); 
-		                     nickInfoIt != m_server->getNicksOnline()->end(); ++nickInfoIt)
+void ServerISON::recalculateAddressees()
+{
+  kdDebug() << "ServerISON::recalculateAddressees" << endl;
+  // Get all nicks known to be online.
+  const NickInfoMap* allNicks = m_server->getAllNicks();
+  // Sync all online nicks with addressbook.
+  for(NickInfoMap::ConstIterator it=allNicks->begin(); it != allNicks->end(); ++it)
+  {
+    NickInfoPtr nickInfo = it.data();
+    nickInfo->refreshAddressee();
+  }
+  // If not watching nicks, no need to build notify list.
+  if (m_useNotify)
+  {
+    // Build a map of online nicknames with associated addressbook entry,
+    // indexed by KABC::Addressee uid.
+    // Note that there can be more than one nick associated with an addressee.
+    QMap<QString,QStringList> addresseeToOnlineNickMap;
+    NickInfoMap::ConstIterator nickInfoItEnd = allNicks->end();
+    for(NickInfoMap::ConstIterator nickInfoIt=allNicks->begin();
+      nickInfoIt != nickInfoItEnd; ++nickInfoIt)
+    {
+      NickInfoPtr nickInfo = nickInfoIt.data();
+      KABC::Addressee addressee = nickInfo->getAddressee();
+      if (!addressee.isEmpty())
       {
-        NickInfoPtr nickInfo = nickInfoIt.data();
-        if(nickInfo->getAddressee().uid() == uid) {
-          m_addresseesISON.append(nickInfo->getNickname());  //They are online.  Use the nickname they are online as, to make a short string.
-  	  found = true;
-        }
+        QString uid = addressee.uid();
+        QStringList nicknames = addresseeToOnlineNickMap[uid];
+        nicknames.append(nickInfo->getNickname());
+        addresseeToOnlineNickMap[uid] = nicknames;
       }
-      if(!found) {
-        QString lserverName = m_server->getServerName().lower();
-        QString lserverGroup = m_server->getServerGroup().lower();
-	//Okay we can't find the nick online, 
-        QStringList nicks = QStringList::split( QChar( 0xE000 ), (*it).custom("messaging/irc", "All") );
-        for( QStringList::Iterator nicksIt = nicks.begin(); nicksIt != nicks.end(); ++nicksIt ) {
-          QString lserverOrGroup = (*nicksIt).section(QChar(0xE120),1).lower();
-          if(lserverOrGroup == lserverName || lserverOrGroup == lserverGroup || lserverOrGroup.isEmpty()) {
-	    QString nickname = (*nicksIt).section(QChar(0xE120),0,0).lower();
-            m_addresseesISON.append(nickname);  //Add all the nicks that are on this server.
+    }
+  
+    // Lowercase server name and server group.
+    QString lserverName = m_server->getServerName().lower();
+    QString lserverGroup = m_server->getServerGroup().lower();
+  
+    // Build notify list from nicks in addressbook.
+    m_addresseesISON.clear();
+    m_offlineNickToAddresseeMap.clear();
+    for( KABC::AddressBook::ConstIterator it = 
+      Konversation::Addressbook::self()->getAddressBook()->begin();
+      it != Konversation::Addressbook::self()->getAddressBook()->end(); ++it )
+    {
+      if(Konversation::Addressbook::self()->hasAnyNicks(*it))
+      {
+        QString uid = (*it).uid();
+        // First check if we already know that this addressee is online.
+        // If so, add all the nicks of the addressee that are online, but do not
+        // add the offline nicks.  There is no point in monitoring such nicks.
+        if (addresseeToOnlineNickMap.contains(uid))
+        {
+          QStringList nicknames = addresseeToOnlineNickMap[uid];
+          for (unsigned int index=0; index<nicknames.count(); index++)
+            m_addresseesISON.append(nicknames[index]);
+        }
+        else
+        {
+          // If addressee is not known to be online, add all of the nicknames
+          // of the addressee associated with this server or server group (if any)
+          // to the notify list.
+          // Simultaneously, build a map of all offline nicks and corresponding
+          // KABC::Addressee, indexed by nickname.
+          QStringList nicks = QStringList::split( QChar( 0xE000 ),
+            (*it).custom("messaging/irc", "All") );
+          for( QStringList::Iterator nicksIt = nicks.begin(); nicksIt != nicks.end(); ++nicksIt )
+          {
+            QString lserverOrGroup = (*nicksIt).section(QChar(0xE120),1).lower();
+            if(lserverOrGroup == lserverName || lserverOrGroup == lserverGroup ||
+              lserverOrGroup.isEmpty())
+            {
+              QString nickname = (*nicksIt).section(QChar(0xE120),0,0).lower();
+              m_addresseesISON.append(nickname);
+              m_offlineNickToAddresseeMap.insert(nickname, *it, true);
+            }
           }
         }
-      }
-    }    
+      }    
+    }
+    // Merge with watch list from prefs.
+    m_ISONList = m_prefsWatchList;
+    for (unsigned int index=0; index<m_addresseesISON.count(); index++)
+    {
+      QString nickname = m_addresseesISON[index];
+      if (!m_ISONList.contains(nickname)) m_ISONList.append(nickname);
+    }
   }
 }
 
-QString ServerISON::getWatchedNicks() {
-  return KonversationApplication::preferences.getNotifyStringByGroup(m_server->getServerGroup());
+// When user changes preferences and has nick watching turned on, rebuild notify list.
+void ServerISON::slotPrefsChanged()
+{
+  kdDebug() << "ServerISON::slotPrefsChanged" << endl;
+  bool useNotify = KonversationApplication::preferences.getUseNotify();
+  if (useNotify)
+  {
+    bool turnedOn = !m_useNotify;
+    m_useNotify = true;
+    // Get (possibly) modified Nick Watch List from preferences.
+    QString groupName = m_server->getServerGroup();
+    m_prefsWatchList =
+      KonversationApplication::preferences.getNotifyListByGroup(groupName);
+    // If user just turned on nick watching, build addressbook watch list
+    // and merge with user prefs watch list.
+    if (turnedOn)
+      recalculateAddressees();
+    else
+    {
+      // Merge (possibly) modified Watch List from preferences with previously
+      // calculated addressbook watch list.
+      m_ISONList = m_prefsWatchList;
+      for (unsigned int index=0; index<m_addresseesISON.count(); index++)
+      {
+        QString nickname = m_addresseesISON[index];
+        if (!m_ISONList.contains(nickname)) m_ISONList.append(nickname);
+      }
+    }
+  }
+  // If nick watching is off, clear list.
+  else m_ISONList.clear();
+  m_useNotify = useNotify;
 }
 
 void ServerISON::nickInfoChanged(Server* /*server*/, const NickInfoPtr /*nickInfo*/) {
-  //TODO only reculate that one nickinfo somehow.. this is such a waste of time :(
-  //not sure how though
+  // TODO only reculate that one nickinfo somehow.. this is such a waste of time :(
+  // not sure how though
+  // I would hope we can get rid of this routine altogther..gary.
   recalculateAddressees();
 }
  
-
 #include "serverison.moc"
 
--- konversation/serverison.h
+++ konversation/serverison.h
@@ -19,71 +19,84 @@
 #define SERVERISON_H
 
 
-/*
-  @author John Tapsell
+/**
+* @author John Tapsell
+* @author Gary Cramblitt <garycramblitt@comcast.net>
 */
 
 class Server;
 
-typedef QMap<KABC::Addressee,QString> AddresseeNickMap;
+typedef QMap<QString,KABC::Addressee> OfflineNickToAddresseeMap;
 
 class ServerISON : public QObject
 {
   Q_OBJECT
 
   public:
-    ServerISON(const Server *const server);
-    /** Returns a list of nicks that we want to know whether they are online
-      * of offline.  This function is called, and the result sent to the
-      * server as an /ISON command.
-      * 
-      * Calls getAddressees() and getWatachedNicks(), and returns the merged result.
-      * The resulting nicks don't have the servername/servergroup attached.
-      *
-      * Read the documentation of the other two functions.
-      * 
-      * @returns A space delimited list of nicks that we want to know if they are on or not.
-      * 
-      * @see getAddressees()
-      * @see getWatchedNicks()
-      */
-    QString getISON();
-    /** Returns _some_ of the nicks that the addressees have.
-     *  It loops through all the addressees that have nickinfos.
-     *
-     *  - If that addressee has some nicks, and at least one of them is in a
-     *    channel we are in, then we know they are online, so don't add.
-     *  - Otherwise, if that addressee has some nicks, and we think they are
-     *    online, add the nick that they are currently online with.  This does
-     *    mean that if they change their nick, they will appear offline for the
-     *    duration between ISON's.
-     *  - Otherwise, add all the nicks we know the addressee has.
-     */
+    ServerISON(Server* server);
+    /**
+    * Returns a list of nicks that we want to know whether they are online
+    * of offline.  This function is called, and the result sent to the
+    * server as an /ISON command.
+    * 
+    * Calls getAddressees() and merges with the Watch List from preferences.
+    * The resulting nicks don't have the servername/servergroup attached.
+    *
+    * @returns              A list of nicks that we want to know if they are on or not.
+    * 
+    * @see getAddressees()
+    */
+    QStringList getISONList();
+    
+    /**
+    * Returns _some_ of the nicks that the addressees have.
+    * It loops through all the addressees that have nickinfos.
+    *
+    *  - If that addressee has some nicks, and at least one of them is in a
+    *    channel we are in, then we know they are online, so don't add.
+    *  - Otherwise, if that addressee has some nicks, and we think they are
+    *    online, add the nick that they are currently online with.  This does
+    *    mean that if they change their nick, they will appear offline for the
+    *    duration between ISON's.
+    *  - Otherwise, add all the nicks we know the addressee has.
+    */
     QStringList getAddressees();
-    /** Looks in the preferences for which nicks the user wants to watch for.
-     *  Filters for only the ones for this server.  Strips off the server
-     *  name/group.
-     */
-    QString getWatchedNicks();
+    
+    /**
+    * Given the nickname of nick that is offline (or at least not known to be online),
+    * returns the addressbook entry (if any) for the nick.
+    * @param nickname       Desired nickname.  Case sensitive.
+    * @return               Addressbook entry of the nick or empty if not found.
+    */
+    KABC::Addressee getOfflineNickAddresse(QString& nickname);
+
+  
   public slots:
     void nickInfoChanged(Server* server, const NickInfoPtr nickInfo);
+    void slotPrefsChanged();
        
   private:
-    /** A pointer to the server we are a member of.
-     */
-    const Server *const m_server;
-    /** A map of the nicks that are on this server,
-     *  mapped against the nick they are using if online,
-     *  TODO: actually use
-     */
-    AddresseeNickMap m_addresseeNickMap;
-
+    /**
+    * Rebuilds list of nicks to watch whenever an addressbook change occurs
+    * or whenever user turns on nick watching.
+    */
     void recalculateAddressees();
-    /** The return result for getAddressees()
-     *  @see getAddressees()
-     *  @return A list of nicks for this server that we need to do /ISON on.
-     */
+    
+    /// Map of all offline nicks in the addressbook associated with this server
+    /// or server group and their addressbook entry, indexed by nickname.
+    /// TODO: Lowercase nickname?
+    OfflineNickToAddresseeMap m_offlineNickToAddresseeMap;
+    
+    /// A pointer to the server we are a member of.
+    Server* m_server;
+    /// List of nicks to watch that come from addressbook.
     QStringList m_addresseesISON;
+    /// List of nicks in the Nick Watch List (from preferences).
+    QStringList m_prefsWatchList;
+    /// Merged list of the two above.
+    QStringList m_ISONList;
+    /// State of UseNotify preference.
+    bool m_useNotify;
 };
 
 
