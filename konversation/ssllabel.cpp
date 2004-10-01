9f355bc789aa4c83fb61958a38f6ba1fecfd002a
İsmail Dönmez <ismail@kde.org>
2004-10-01 15:45:39 +0000

    CVS_SILENT Small changes
    svn path=/trunk/kdeextragear-2/konversation/; revision=350868

--- konversation/ssllabel.cpp
+++ konversation/ssllabel.cpp
@@ -1,3 +1,17 @@
+/*
+  Copyright (c) 2004 by İsmail Dönmez <ismail.donmez@boun.edu.tr>
+
+  *************************************************************************
+  *                                                                       *
+  * This program is free software; you can redistribute it and/or modify  *
+  * it under the terms of the GNU General Public License as published by  *
+  * the Free Software Foundation; either version 2 of the License, or     *
+  * (at your option) any later version.                                   *
+  *                                                                       *
+  *************************************************************************
+
+*/
+
 #include "ssllabel.h"
 
 SSLLabel::SSLLabel(QWidget* parent,const char* name)
--- konversation/ssllabel.h
+++ konversation/ssllabel.h
@@ -1,6 +1,20 @@
 #ifndef SSLLABEL_H
 #define SSLLABEL_H
 
+/*
+  Copyright (c) 2004 by İsmail Dönmez <ismail.donmez@boun.edu.tr>
+  
+  *************************************************************************
+  *                                                                       *
+  * This program is free software; you can redistribute it and/or modify  *
+  * it under the terms of the GNU General Public License as published by  *
+  * the Free Software Foundation; either version 2 of the License, or     *
+  * (at your option) any later version.                                   *
+  *                                                                       *
+  *************************************************************************
+  
+*/
+
 #include <qlabel.h>
 
 class SSLLabel : public QLabel
