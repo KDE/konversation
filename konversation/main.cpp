c45436374b1c7283220c6dc2e99866ef6d9cad85
Bram Schoenmakers <bramschoenmakers@kde.nl>
2004-10-12 15:16:20 +0000

    CVS_SILENT Typo.
    svn path=/trunk/kdeextragear-2/konversation/; revision=353963

--- konversation/main.cpp
+++ konversation/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
   aboutData.addAuthor("Mickael Marchand",I18N_NOOP("Konsole part view"),"marchand@kde.org");
   aboutData.addAuthor("Peter Simonsson",I18N_NOOP("Color picker, IRC color preferences, KNotify events, Systray notification, Shell style completion, Sound support for highlights"),"psn@linux.se");
   aboutData.addAuthor("Christian Muehlhaeuser",I18N_NOOP("Multiple modes extension, Close widget placement, OSD functionality"),"chris@chris.de");
-  aboutData.addAuthor("John Tapsell",I18N_NOOP("Refactoring, Kadddressbook/Kontact integration"), "john@geola.co.uk");
+  aboutData.addAuthor("John Tapsell",I18N_NOOP("Refactoring, KAddressBook/Kontact integration"), "john@geola.co.uk");
   aboutData.addAuthor("İsmail Dönmez",I18N_NOOP("SSL Support, Ported to KNetwork"),"ismail.donmez@boun.edu.tr");
   aboutData.addAuthor("Shintaro Matsuoka",I18N_NOOP("DCC rewrite, Encodings improvements, OSD Positioning"),"shin@shoegazed.org");
 
