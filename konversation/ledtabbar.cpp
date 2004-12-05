bc0042bd39dba848014efb6c3a5f33cc938bf0d6
İsmail Dönmez <ismail@kde.org>
2004-12-05 08:37:15 +0000

    s/SmallIcon/SmallIconSet
    svn path=/trunk/kdeextragear-2/konversation/; revision=368582

--- konversation/ledtabbar.cpp
+++ konversation/ledtabbar.cpp
@@ -99,8 +99,8 @@ LedTabBar::LedTabBar(QWidget* parent,const char* name) :
     // }
 
     m_popup->insertTitle(i18n("Tab"),Label);
-    m_popup->insertItem(SmallIcon("1leftarrow"), i18n("Move &Left"),MoveLeft);
-    m_popup->insertItem(SmallIcon("1rightarrow"), i18n("Move &Right"),MoveRight);
+    m_popup->insertItem(SmallIconSet("1leftarrow"), i18n("Move &Left"),MoveLeft);
+    m_popup->insertItem(SmallIconSet("1rightarrow"), i18n("Move &Right"),MoveRight);
     m_popup->insertSeparator();
     m_popup->insertItem(SmallIcon("charset"),i18n("Set &Encoding"),m_popupEncoding,EncodingSub);
     m_popup->insertItem(SmallIconSet("mail_generic"),i18n("&Send Email..."),SendEmail);
