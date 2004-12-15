9b82ae96650fea6849529142663c2586fda701e7
İsmail Dönmez <ismail@kde.org>
2004-12-15 20:35:51 +0000

    CVS_SILENT Revert last
    svn path=/trunk/kdeextragear-2/konversation/; revision=370858

--- konversation/nicksonline.h
+++ konversation/nicksonline.h
@@ -23,9 +23,7 @@
 // Konversation includes.
 #include "nickinfo.h"
 #include "chatwindow.h"
-#include "channel.h"
 #include "linkaddressbook/nicksonlinetooltip.h"
-
 /**
 *  @author Dario Abatianni
 *  @author Gary Cramblitt <garycramblitt@comcast.net>
