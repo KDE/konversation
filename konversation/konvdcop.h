faaeb611c0a3ff35dac6fe8278a0cb90ac2798b2
Benjamin Meyer <ben@meyerhome.net>
2004-12-19 19:40:31 +0000

    added license/copyright from konvdcop.cpp
    svn path=/trunk/kdeextragear-2/konversation/; revision=372015

--- konversation/konvdcop.h
+++ konversation/konvdcop.h
@@ -1,3 +1,17 @@
+/*
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
+/*
+  The konversation DCOP interface class
+  begin:     Mar 7 2003
+  copyright: (C) 2003 by Alex Zepeda
+  email:     zipzippy@sonic.net
+*/
+
 #ifndef KONV_DCOP_H
 #define KONV_DCOP_H
 
