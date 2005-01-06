932ef1e1e11d227b9d30aa02ae08f4898c9fbefd
Peter Simonsson <peter.simonsson@gmail.com>
2005-01-06 18:54:38 +0000

    Some minor code clean up... couldn't help myself as it was a bit hard to read the code in some places
    svn path=/trunk/kdeextragear-2/konversation/; revision=375921

--- konversation/nicklistviewitem.cpp
+++ konversation/nicklistviewitem.cpp
@@ -47,8 +47,10 @@ void NickListViewItem::refresh()
   bool needResort = false;
   NickInfo* nickInfo = nick->getNickInfo();
   bool away = false;
-  if ( nickInfo )
+  
+  if ( nickInfo ) {
     away = nickInfo->isAway();
+  }
   
   if(away != m_away) {
     m_away = away;
@@ -58,71 +60,88 @@ void NickListViewItem::refresh()
   Images* images = KonversationApplication::instance()->images();
   QPixmap icon;
   
-  if ( nick->isAdmin() )
+  if ( nick->isAdmin() ) {
     icon = images->getNickIcon( Images::Admin, away );
-  else if ( nick->isOwner() )
+  } else if ( nick->isOwner() ) {
     icon = images->getNickIcon( Images::Owner, away );
-  else if ( nick->isOp() )
+  } else if ( nick->isOp() ) {
     icon = images->getNickIcon( Images::Op, away );
-  else if ( nick->isHalfop() )
+  } else if ( nick->isHalfop() ) {
     icon = images->getNickIcon( Images::HalfOp, away );
-  else if ( nick->hasVoice() )
+  } else if ( nick->hasVoice() ) {
     icon = images->getNickIcon( Images::Voice, away );
-  else
+  } else {
     icon = images->getNickIcon( Images::Normal, away );
+  }
   
   setPixmap( 0, icon );
   
   KABC::Picture pic = nickInfo->getAddressee().photo();
-  if(!pic.isIntern())
+
+  if(!pic.isIntern()) {
     pic = nickInfo->getAddressee().logo();
+  }
+
   if(pic.isIntern())
   {
     QPixmap qpixmap(pic.data().scaleHeight(m_height));
     setPixmap(1,qpixmap);
   }
+
   QString newtext1 = calculateLabel1();
+
   if(newtext1 != text(1)) {
     setText(1,calculateLabel1());
     needResort = true;
   }
+
   setText(2,calculateLabel2());
   repaint();
-  
-  if(needResort)
+
+  if(needResort) {
     emit refreshed(); // Resort nick list
+  }
 }
 
 QString NickListViewItem::calculateLabel1() {
   NickInfoPtr nickinfo = nick->getNickInfo();
   KABC::Addressee addressee = nickinfo->getAddressee();
-  if(!addressee.realName().isEmpty()) //if no addressee, realName will be empty
+
+  if(!addressee.realName().isEmpty()) {//if no addressee, realName will be empty
     return nick->getNickInfo()->getNickname() + " (" + addressee.realName() + ")";
+  }
+
   return nick->getNickInfo()->getNickname();
 }
+
 QString NickListViewItem::calculateLabel2() {
   return nick->getNickInfo()->getHostmask();
 }
+
 int NickListViewItem::compare(QListViewItem* item,int col,bool ascending) const
 {
-  NickListViewItem* otherItem=static_cast<NickListViewItem*>(item);
-
-  int thisFlags=getFlags();
-  int otherFlags=otherItem->getFlags();
+  NickListViewItem* otherItem = static_cast<NickListViewItem*>(item);
 
   if(KonversationApplication::preferences.getSortByStatus())
   {
-    if(thisFlags>otherFlags) return 1;
-    if(thisFlags<otherFlags) return -1;
+    int thisFlags = getFlags();
+    int otherFlags = otherItem->getFlags();
+
+    if(thisFlags > otherFlags) {
+      return 1;
+    }
+    if(thisFlags < otherFlags) {
+      return -1;
+    }
   }
 
-  QString thisKey=key(col,ascending);
-  QString otherKey=otherItem->key(col,ascending);
+  QString thisKey = key(col, ascending);
+  QString otherKey = otherItem->key(col, ascending);
 
   if(KonversationApplication::preferences.getSortCaseInsensitive())
   {
-    thisKey=thisKey.lower();
-    otherKey=otherKey.lower();
+    thisKey = thisKey.lower();
+    otherKey = otherKey.lower();
   }
 
   return thisKey.compare(otherKey);
@@ -133,13 +152,21 @@ int NickListViewItem::getFlags() const
   NickInfo* nickInfo = nick->getNickInfo();
   int flags;
 
-  if(nick->isAdmin())           flags=KonversationApplication::preferences.getAdminValue();
-  else if(nick->isOwner())      flags=KonversationApplication::preferences.getOwnerValue();
-  else if(nick->isOp())         flags=KonversationApplication::preferences.getOpValue();
-  else if(nick->isHalfop())     flags=KonversationApplication::preferences.getHalfopValue();
-  else if(nick->hasVoice())     flags=KonversationApplication::preferences.getVoiceValue();
-  else if(nickInfo->isAway())   flags=KonversationApplication::preferences.getAwayValue();
-  else                          flags=KonversationApplication::preferences.getNoRightsValue();
+  if(nick->isAdmin()) {
+    flags = KonversationApplication::preferences.getAdminValue();
+  } else if(nick->isOwner()) {
+    flags = KonversationApplication::preferences.getOwnerValue();
+  } else if(nick->isOp()) {
+    flags = KonversationApplication::preferences.getOpValue();
+  } else if(nick->isHalfop()) {
+    flags = KonversationApplication::preferences.getHalfopValue();
+  } else if(nick->hasVoice()) {
+    flags = KonversationApplication::preferences.getVoiceValue();
+  } else if(nickInfo->isAway()) {
+    flags = KonversationApplication::preferences.getAwayValue();
+  } else {
+    flags = KonversationApplication::preferences.getNoRightsValue();
+  }
 
   return flags;
 }
