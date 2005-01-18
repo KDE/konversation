28f77391bb239906b380f1327a4ec352fc9847ee
İsmail Dönmez <ismail@kde.org>
2005-01-18 20:54:37 +0000

    Add segfault2k to credits "
    svn path=/trunk/kdeextragear-2/konversation/; revision=379891

--- konversation/main.cpp
+++ konversation/main.cpp
@@ -57,6 +57,7 @@ int main(int argc, char* argv[])
   aboutData.addAuthor("Mickael Marchand",I18N_NOOP("Konsole part view"),"marchand@kde.org");
   
   aboutData.addCredit("Benjamin Meyer",I18N_NOOP("A Handfull of fixes and code cleanup."),"ben+konversation@meyerhome.net");
+  aboutData.addCredit("Sebastian Sariego",I18N_NOOP("Artwork"),"segfault@kde.cl");
   aboutData.addCredit("Frauke Oster",I18N_NOOP("System tray patch"),"frauke@frsv.de");
   aboutData.addCredit("Lucijan Busch",I18N_NOOP("Bug fixes"),"lucijan@kde.org");
   aboutData.addCredit("Sascha Cunz",I18N_NOOP("Extended user modes patch"),"mail@sacu.de");
@@ -65,7 +66,6 @@ int main(int argc, char* argv[])
   aboutData.addCredit("Tobias Olry",I18N_NOOP("Channel ownership mode patch"),"tobias.olry@web.de");
   aboutData.addCredit("Ruud Nabben",I18N_NOOP("Option to enable IRC color filtering"),"r.nabben@gawab.com");
   aboutData.addCredit("Michael Goettsche",I18N_NOOP("Quick connect, Ported new OSD, other features and bugfixes"),"michael.goettsche@kdemail.net");
-  aboutData.addCredit("Luciash d' being",I18N_NOOP("Application icons"),"luci@sh.ground.cz");
 
 
   KCmdLineArgs::init(argc,argv,&aboutData);
