7c0e63823ec44bd7c06487a88334362a2dd730b8
İsmail Dönmez <ismail@kde.org>
2005-01-28 18:05:31 +0000

    CVS_SILENT compile
    svn path=/trunk/kdeextragear-2/konversation/; revision=383349

--- konversation/main.cpp
+++ konversation/main.cpp
@@ -58,7 +58,7 @@ int main(int argc, char* argv[])
   
   aboutData.addCredit("Michael Goettsche",I18N_NOOP("Quick connect, Ported new OSD, other features and bugfixes"),"michael.goettsche@kdemail.net");
   aboutData.addCredit("Benjamin Meyer",I18N_NOOP("A Handfull of fixes and code cleanup"),"ben+konversation@meyerhome.net");
-  aboutData.addCredit("Jakub Stachowski",I18N_NOOP("Drag&Drop improvements","qbast@go2.pl");
+  aboutData.addCredit("Jakub Stachowski",I18N_NOOP("Drag&Drop improvements"),"qbast@go2.pl");
   aboutData.addCredit("Sebastian Sariego",I18N_NOOP("Artwork"),"segfault@kde.cl");
   aboutData.addCredit("Frauke Oster",I18N_NOOP("System tray patch"),"frauke@frsv.de");
   aboutData.addCredit("Lucijan Busch",I18N_NOOP("Bug fixes"),"lucijan@kde.org");
