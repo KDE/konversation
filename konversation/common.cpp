39d2b550c15edc0b6c8ce741b97fd80ab5b04cc4
Peter Simonsson <peter.simonsson@gmail.com>
2005-02-27 01:56:51 +0000

    Allow '*' in urls
    svn path=/trunk/kdeextragear-2/konversation/; revision=393414

--- konversation/common.cpp
+++ konversation/common.cpp
@@ -66,7 +66,7 @@ QString tagURLs(const QString& text, const QString& fromNick)
   pos = 0;
   urlLen =0;
 
-  QRegExp urlPattern("((www\\.(?!\\.)|(fish|(f|ht)tp(|s))://)[\\d\\w\\./,\\':~\\?=&;#@\\-\\+\\%]+[\\d\\w/])|"
+  QRegExp urlPattern("((www\\.(?!\\.)|(fish|(f|ht)tp(|s))://)[\\d\\w\\./,\\':~\\?=&;#@\\-\\+\\%\\*]+[\\d\\w/])|"
       "([-.\\d\\w]+@[-.\\d\\w]{2,}\\.[\\w]{2,})");
   urlPattern.setCaseSensitive(false);
 
