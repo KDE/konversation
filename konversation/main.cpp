4a8777532d934098fbe39a7cf7fce76e2c0bc16d
İsmail Dönmez <ismail@kde.org>
2005-03-22 08:16:17 +0000

    CVS_SILENT Update
    svn path=/trunk/kdeextragear-2/konversation/; revision=399630

--- konversation/main.cpp
+++ konversation/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
                                  "talk about your favorite subject."),
                        "http://www.konversation.org/");
 
-  aboutData.addAuthor("Dario Abatianni",I18N_NOOP("Project founder, main programmer, release coordinator"),"eisfuchs@tigress.com");
+  aboutData.addAuthor("Dario Abatianni",I18N_NOOP("Project founder, main programmer"),"eisfuchs@tigress.com");
   aboutData.addAuthor("Peter Simonsson",I18N_NOOP("Color picker, IRC color preferences, KNotify events, Systray notification, Shell style completion, Sound support for highlights"),"psn@linux.se");
   aboutData.addAuthor("Christian Muehlhaeuser",I18N_NOOP("Multiple modes extension, Close widget placement, OSD functionality"),"chris@chris.de");
   aboutData.addAuthor("John Tapsell",I18N_NOOP("Refactoring, KAddressBook/Kontact integration"), "john@geola.co.uk");
