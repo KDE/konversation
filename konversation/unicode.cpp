c30a1469196175f06a4c99095acb30681226b2de
İsmail Dönmez <ismail@kde.org>
2005-03-31 14:22:10 +0000

    CVS_SILENT Bump commit.h & add my copyright so Japanese chixors can love me
    svn path=/trunk/kdeextragear-2/konversation/; revision=402161

--- konversation/unicode.cpp
+++ konversation/unicode.cpp
@@ -16,7 +16,7 @@
 */
 
 /* See http://lxr.mozilla.org/mozilla/source/modules/rdf/src/utils.c#540 */
-/* Modified by İsmail Dönmez <ismail@kde.org.tr> (C) 2005 */
+/* Modified by İsmail Dönmez <ismail@kde.org.tr> (C) 2005 for Konversation */
 
 #define kLeft1BitMask  0x80
 #define kLeft2BitsMask 0xC0
