11110a8c22ef918355862b2c3144c39b55c7dbbc
İsmail Dönmez <ismail@kde.org>
2005-04-08 11:17:05 +0000

    ALT+0 for 10th tab. Suggestion by thiago CCBUG:103394 CCMAIL:thiago@kde.org.tr
    svn path=/trunk/kdeextragear-2/konversation/; revision=404024

--- konversation/konversationmainwindow.cpp
+++ konversation/konversationmainwindow.cpp
@@ -163,12 +163,12 @@ KonversationMainWindow::KonversationMainWindow() : KMainWindow(0,"main_window", 
   QSignalMapper* tabSelectionMapper = new QSignalMapper(this);
   connect(tabSelectionMapper, SIGNAL(mapped(int)), this, SLOT(goToTab(int)));
   
-  for(uint i = 1; i < 10; ++i) {
-    KAction* tabSelectionAction = new KAction(i18n("Go to Tab %1").arg(i), 0, KShortcut(QString("Alt+%1").arg(i)),
+  for(uint i = 1; i <= 10; ++i) {
+    KAction* tabSelectionAction = new KAction(i18n("Go to Tab %1").arg(i), 0, KShortcut(QString("Alt+%1").arg(i%10)),
       tabSelectionMapper, SLOT(map()), actionCollection(), QString("go_to_tab_%1").arg(i).local8Bit());
     tabSelectionMapper->setMapping( tabSelectionAction, i-1);
   }
-  
+
   action = new KAction(i18n("&Clear Window"),0,KShortcut("Ctrl+L"),this,SLOT(clearWindow()),actionCollection(),"clear_window");
   action->setToolTip(i18n("Clear content of current window"));
   action->setEnabled(false);
