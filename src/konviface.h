b07c46ad3c1de86b700dbeca45b3fe8651dcca53
John Tapsell <john.tapsell@kdemail.net>
2005-05-30 15:45:45 +0000

    Add copyright notice
    svn path=/trunk/extragear/network/konversation/; revision=419767

--- src/konviface.h
+++ src/konviface.h
@@ -1,3 +1,15 @@
+/*
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
+/*
+  copyright: (C) 2005 by Dario Abatianni
+  email:     eisfuchs@tigress.com
+*/
+
 #ifndef KONV_IFACE_H
 #define KONV_IFACE_H
 
