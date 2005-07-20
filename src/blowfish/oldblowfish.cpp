d9b9c680263b763ed6ecd775235624d55df6977d
Dirk Mueller <mueller@kde.org>
2005-07-20 17:51:23 +0000

    SVN_SILENT: update FSF address
    svn path=/trunk/extragear/network/konversation/; revision=436987

--- src/blowfish/oldblowfish.cpp
+++ src/blowfish/oldblowfish.cpp
@@ -10,7 +10,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA 02110-1301  USA
 //
 // Blowfish algorythms: Bruce Schneier and Jim Conger
 //   Bruce Schneier, 1996, Applied Cryptography, 2nd ed., John Wiley & Sons
