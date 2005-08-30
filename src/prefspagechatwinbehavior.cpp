d4cdccfe4991ea8b28fe5222a5bc7c24f29e0b39
İsmail Dönmez <ismail@kde.org>
2005-08-30 14:29:43 +0000

    Fix some crashes by fixing variable names
    svn path=/trunk/extragear/network/konversation/; revision=455062

--- src/prefspagechatwinbehavior.cpp
+++ src/prefspagechatwinbehavior.cpp
@@ -27,7 +27,7 @@ PrefsPageChatWinBehavior::PrefsPageChatWinBehavior(QWidget* newParent, Preferenc
 
     kcfg_BeepOnAsciiBel->setChecked(preferences->getBeep());
     kcfg_HideJoinPart->setChecked(preferences->getHideUnimportantEvents());
-    kcfg_DisableExpansions->setChecked(preferences->getDisableExpansion());
+    kcfg_DisableExpansion->setChecked(preferences->getDisableExpansion());
     kcfg_ShowRememberLine->setChecked(preferences->getShowRememberLineInAllWindows());
     kcfg_RedirectStatusMessages->setChecked(preferences->getRedirectToStatusPane());
     kcfg_UseLiteralModes->setChecked(preferences->getUseLiteralModes());
@@ -35,7 +35,7 @@ PrefsPageChatWinBehavior::PrefsPageChatWinBehavior(QWidget* newParent, Preferenc
     kcfg_ScrollBackLimit->setValue(preferences->getScrollbackMax());
     kcfg_AutoWhoLimit->setValue(preferences->getAutoWhoNicksLimit());
 
-    kcfg_autoWhoContinuous->setChecked(preferences->getAutoWhoContinuousEnabled());
+    kcfg_AutoWhoContinuousEnabled->setChecked(preferences->getAutoWhoContinuousEnabled());
     kcfg_WhoInterval->setValue(preferences->getAutoWhoContinuousInterval());
 
     kcfg_ShowRealNames->setChecked(preferences->getShowRealNames());
@@ -49,13 +49,13 @@ void PrefsPageChatWinBehavior::applyPreferences()
 {
     preferences->setBeep(kcfg_BeepOnAsciiBel->isChecked());
     preferences->setHideUnimportantEvents(kcfg_HideJoinPart->isChecked());
-    preferences->setDisableExpansion(kcfg_DisableExpansions->isChecked());
+    preferences->setDisableExpansion(kcfg_DisableExpansion->isChecked());
     preferences->setShowRememberLineInAllWindows(kcfg_ShowRememberLine->isChecked());
     preferences->setRedirectToStatusPane(kcfg_RedirectStatusMessages->isChecked());
     preferences->setUseLiteralModes(kcfg_UseLiteralModes->isChecked());
     preferences->setScrollbackMax(kcfg_ScrollBackLimit->value());
     preferences->setAutoWhoNicksLimit(kcfg_AutoWhoLimit->value());
-    preferences->setAutoWhoContinuousEnabled(kcfg_autoWhoContinuous->isChecked());
+    preferences->setAutoWhoContinuousEnabled(kcfg_AutoWhoContinuousEnabled->isChecked());
     preferences->setAutoWhoContinuousInterval(kcfg_WhoInterval->value());
     preferences->setShowRealNames(kcfg_ShowRealNames->isChecked());
 }
