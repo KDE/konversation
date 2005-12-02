a9486e6c403f2810792ef1af1f50849eff3bb9b3
John Tapsell <john.tapsell@kdemail.net>
2005-12-02 17:11:18 +0000

    Implement the warnings dialog.  Doesn't actually save the changes when you hit apply/ok, but does show the current settings.
    svn path=/trunk/extragear/network/konversation/; revision=485038

---
+++ src/warnings_preferences.cpp
@@ -0,0 +1,52 @@
+#include <klocale.h>
+#include <klistview.h>
+#include <qlistview.h>
+#include "warnings_preferences.h"
+#include <kapplication.h>
+#include <kconfig.h>
+
+Warnings_Config::Warnings_Config( QWidget* parent, const char* name, WFlags fl )
+    : Warnings_ConfigUI( parent, name, fl )
+{
+  languageChange();
+}
+
+Warnings_Config::~Warnings_Config()
+{
+}
+
+/*
+ *  Sets the strings of the subwidgets using the current
+ *  language.
+ */
+void Warnings_Config::languageChange()
+{
+
+  QStringList dialogDefinitions;
+  flagNames = "Invitation,SaveLogfileNote,ClearLogfileQuestion,CloseQueryAfterIgnore,ResumeTransfer,QuitServerTab,ChannelListNoServerSelected,RemoveDCCReceivedFile,HideMenuBarWarning,ChannelListWarning,LargePaste";
+  dialogDefinitions.append(i18n("Automatically join channel on invite"));
+  dialogDefinitions.append(i18n("Notice that saving logfiles will save whole file"));
+  dialogDefinitions.append(i18n("Question before deleting logfile contents"));
+  dialogDefinitions.append(i18n("Question on closing queries after ignoring the nickname"));
+  dialogDefinitions.append(i18n("Question on what to do on DCC resume"));
+  dialogDefinitions.append(i18n("Quit server when you hit the tab's close button"));
+  dialogDefinitions.append(i18n("The channel list can only be opened from server-aware tabs"));
+  dialogDefinitions.append(i18n("Warning on deleting file received on DCC"));
+  dialogDefinitions.append(i18n("Warning on hiding the main window menu"));
+  dialogDefinitions.append(i18n("Warning on high traffic with channel list"));
+  dialogDefinitions.append(i18n("Warning on pasting large portions of text"));
+  QCheckListItem *item;
+  dialogListView->clear();
+
+  KConfig* config = kapp->config();
+  config->setGroup("Notification Messages");
+
+  for(unsigned int index=0; index<dialogDefinitions.count() ;index++)
+  {
+    item=new QCheckListItem(dialogListView,dialogDefinitions[index],QCheckListItem::CheckBox);
+    item->setText(1,dialogDefinitions[index]);
+    item->setOn(config->readBoolEntry(flagNames.section(",",index,index), true));
+  }
+}
+
+#include "warnings_preferences.moc"
---
+++ src/warnings_preferences.h
@@ -0,0 +1,24 @@
+
+#ifndef WARNINGS_CONFIG_H
+#define WARNINGS_CONFIG_H
+
+#include "warnings_preferencesui.h"
+
+class KListView;
+class QListViewItem;
+
+class Warnings_Config : public Warnings_ConfigUI
+{
+    Q_OBJECT
+
+public:
+    Warnings_Config( QWidget* parent = 0, const char* name = 0, WFlags fl = 0 );
+    ~Warnings_Config();
+    QString flagNames;
+
+protected slots:
+    virtual void languageChange();
+
+};
+
+#endif // WARNINGS_CONFIG_H
