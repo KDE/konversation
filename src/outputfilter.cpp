16db1f580a548bbb21180b0182646d26e62fceff
Eike Hein <hein@kde.org>
2006-01-06 17:40:27 +0000

    Have another go at 3.4 compatibility in the reverse resolve codepath.
    svn path=/trunk/extragear/network/konversation/; revision=494969

--- src/outputfilter.cpp
+++ src/outputfilter.cpp
@@ -1511,8 +1511,7 @@ namespace Konversation
             // Parameter is an IP address
             if (address.isIPv4Addr() || address.isIPv6Addr())
             {
-#if KDE_IS_VERSION(3,5,0)
-                KInetSocketAddress socketAddress(address,0);
+                KNetwork::KInetSocketAddress socketAddress(address,0);
                 QString resolvedTarget;
                 QString serv; // We don't need this, but KReverseResolver::resolve does
 
@@ -1526,9 +1525,6 @@ namespace Konversation
                 {
                     result = error(i18n("Unable to resolve %1").arg(target));
                 }
-#else
-                result = error(i18n("Reverse-resolving requires KDE version 3.5 or higher."));
-#endif
             }
             // Parameter is presumed to be a host due to containing a dot. Yeah, it's dumb.
             // FIXME: The reason we detect the host by occurence of a dot is the large penalty
