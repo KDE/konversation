7625fb196b6a5b7abb3f9465ef855579f04ecd83
Emil Obermayr <nobs@tigress.com>
2006-01-20 23:02:07 +0000

    making nice and documented
    svn path=/trunk/extragear/network/konversation/; revision=500692

--- src/konversation-0.19-notify2.pl
+++ src/konversation-0.19-notify2.pl
@@ -1,8 +1,28 @@
 #!/usr/bin/perl
 
+# updater from notify in konversation 0.18 syntax to 0.19
+
+# might also become framework for similar tasks
+
+# for use in KDE UPD
+
+# http://wiki.kdenews.org/tiki-index.php?page=Updating+User+Configuration+Files
+
+# (C) 2006 Emil 'nobs' Obermayr, started 2006-01-20
+
+use strict;
+
+# define all variables needed
+
+my ($group, $key, $value, %servername, %servernewnicks, %serveroldnicks, $nicks, %nicksA, $key2);
+
 while (<>){
 
     chomp;
+
+#
+# decide what kind of line we got
+#
 
     if (/^\s*$/){
 # empty line
@@ -12,6 +32,7 @@ while (<>){
 	next;
     } elsif (/\[(.*)\]/){
 # group
+# handle end-of-group here if needed
 	$group=$1;
 	next;
     } elsif  (/([^=]*)=(.*)/) {
@@ -23,29 +44,40 @@ while (<>){
 	next;
     }
 
+#
+# collect data from current line
+#
+
     if ($group =~ /ServerGroup/){
+# we are in a server group in new syntax
+# collecting name of server group name and already included notifies
 	if ($key eq "Name"){
 	    $servername{$group}=$value;
-	}
-	if ($key eq "NotifyList"){
+	} elsif ($key eq "NotifyList"){
 	    $servernewnicks{$group}=$value;
 	}
-    }   
-
-    if ($group =~ /Notify Group Lists/){
+    } elsif ($group =~ /Notify Group Lists/){
+# we are in notify group of old syntax
+# key is the server group name, value are the nick
 	$serveroldnicks{$key}=$value;
     }
 }
+
+#
+# processing collected data
+#
 
 foreach $key (keys %servername) {
     $value = $servername{$key}; 
 
 # servername: $key
-# serverID $value
+# serverID:   $value
 
     $nicks=$serveroldnicks{$value} ." ". $servernewnicks{$key};
 
     undef %nicksA;
+
+# putting nicks in hash as keys, automagically make uniq
 
     while ($nicks =~ /([\S]+)\s*/g){
 	$nicksA{$1}="1";
@@ -53,9 +85,13 @@ foreach $key (keys %servername) {
 
     undef $nicks;
 
+# rebuild string from nick-hash, lower-case and sorted
+
     foreach $key2 (sort map {lc} keys %nicksA){
 	$nicks .= "$key2 ";
     }    
+
+# output stuff in KDE-UPD-syntax
 
     print "
 # DELETE [Notify Group Lists]$value
