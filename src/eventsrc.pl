7a1b5bcb347886e94145acb499494a03ab99252d
Emil Obermayr <nobs@tigress.com>
2006-01-26 11:15:36 +0000

    added some statistics over all languages with dummy language xxx
    svn path=/trunk/extragear/network/konversation/; revision=502514

--- src/eventsrc.pl
+++ src/eventsrc.pl
@@ -7,20 +7,25 @@
 
 # (C) 2006 Emil 'nobs' Obermayr
 
-($file,$lang,$all)=@ARGV;
+($file,$lang,$all,$dummy)=@ARGV;
 
-if($lang =~ /^[a-z]{2}$/){
-    print "searching for missing translations to $lang\n";
+if ($lang =~ /^[a-z]{2}$/){
+    print "searching for missing translations to $lang\n\n";
+    $stat=0;
+} elsif ($lang eq "xxx") {
+    print "giving statistics over all languages\n\n";
+    $stat=1;
 } else {
     print "
 eventsrc.pl searches for missing translations
 
 usage: eventsrc.pl <file> <lang> [all]
 
-file: eventsrc-file in /src/
+file: eventsrc-filename, normalley 'eventsrc'
 
 lang: 2 lower-case letters; language-code
       e.g. de for german, fr for french
+      use xxx for a overall statistic
 
 all:  set to 1 for proofreading (show all)
 ";
@@ -29,11 +34,13 @@ all:  set to 1 for proofreading (show all)
 
 open file;
 
+$found=1;
+
 while (<file>){
     chomp;
 
     if (/^([^\[=]*)=(.*)$/){
-	if ($found==0){
+	if ($found==0 && $stat==0){
 	    print "[$group] $key: $english -> missing\n";
 	} elsif ($found==1 && $all==1){
 	    print "[$group] $key: $english -> $trans\n";
@@ -46,13 +53,29 @@ while (<file>){
 	# uninteressante keys als gefunden markieren
 	if ($key eq "default_presentation"){
 	    $found=2;
-	} else {}
+	} else {
+	    $count++;
+	}
 
-    } elsif (/^${key}\[${lang}\]=(.*)/){
-	$trans=$1;
-	$found=1;
+    } elsif (/^${key}\[(..)\]=(.*)/){
+	$countA{$1}++;
+
+	if ($1 eq $lang){
+	    $trans=$2;
+	    $found=1;
+	} else{}
     } elsif (/^\[(.*)\]$/){
 	$group=$1;
-    } else {}
-    
+    } else {}    
 }
+
+if ($stat==1){
+    foreach $lang (sort keys %countA){
+	$tmp=$countA{$lang};
+	print "In language $lang $tmp / $count are translated.\n";
+    }
+}else{
+    $tmp=$countA{$lang};
+    print "\nIn language $lang $tmp / $count are translated.\n";
+}
+
