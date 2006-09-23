e883127d1ddbd60a56aaf0fa7b026f32dc0131d7
Eike Hein <hein@kde.org>
2006-09-23 22:00:23 +0000

    License fix.
    svn path=/trunk/extragear/network/konversation/; revision=587744

--- src/blowfish/oldblowfish2.h
+++ src/blowfish/oldblowfish2.h
@@ -1,12 +1,12 @@
 /*
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-*/
+  blowfish1.h     interface file for mc_blowfish.cpp
+  _THE BLOWFISH ENCRYPTION ALGORITHM_
+  by Bruce Schneier, 1996, Applied Cryptography, 2nd ed., John Wiley & Sons
 
-/*
-  Copyright (C) 2005 Ismail Donmez <ismail@kde.org>
+  Public Domain.
+
+  Copyright (C) 1994 Bruce Schneier <schneier@counterpane.com>
+  Copyright (C) 1996 Jim Conger <jconger@cox.net>
 */
 
 // blowfish.h2  header file containing random number tables
