f266699978a3003ff86e2855293cae1e88f408e5
Eike Hein <hein@kde.org>
2006-10-06 18:10:47 +0000

    Bump version and build number, add build number back to About and CTCP version reply, bootstrap the changelog for post-1.0.1 changes.
    svn path=/trunk/extragear/network/konversation/; revision=593072

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,9 @@
 #include "version.h"
 #include "commit.h"
 
+#define HACKSTR(x) #x
+#define STRHACK(x) HACKSTR(x)
+
 /*
   Don't use i18n() here, use I18N_NOOP() instead!
   i18n() will only work as soon as a kapplication object was made.
@@ -46,7 +49,7 @@ int main(int argc, char* argv[])
 {
     KAboutData aboutData("konversation",
         I18N_NOOP("Konversation"),
-        KONVI_VERSION,
+        KONVI_VERSION " #" STRHACK(COMMIT),
         shortDescription,
         KAboutData::License_GPL,
         I18N_NOOP("(C) 2002-2006 by the Konversation team"),
