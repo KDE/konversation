KDE/konversation

commit 135c30ffe80b90ace00b72060a24ab7cc83d5934

Use the nick color for queries
svn path=/trunk/extragear/network/konversation/; revision=609405

src/ircview.cpp
@@ -732,7 +732,7 @@ void IRCView::appendQuery(const QString& nick,const QString& message)
     }
 
     QString line;
-    m_tabNotification = Konversation::tnfNormal;
+    m_tabNotification = Konversation::tnfNick;
 
     QString nickLine = createNickLine(nick);
 

commit 998686118c03a570140564955332c80569eae21d

Fix bug introduced by r609405 which circumvents the logic of the tab coloring system as exposed by the preferences. The "Highlight" tab color is used for, well, highlights, which are configured in the appropriate settings page, including the special case "current nick" highlight. Saying query lines always magically include the current nick without them doing to is unpredictable and breaks the mechanics of highlighting based on line contents.
svn path=/trunk/extragear/network/konversation/; revision=609408

src/ircview.cpp
@@ -732,7 +732,7 @@ void IRCView::appendQuery(const QString& nick,const QString& message)
     }
 
     QString line;
-    m_tabNotification = Konversation::tnfNick;
+    m_tabNotification = Konversation::tnfNormal;
 
     QString nickLine = createNickLine(nick);
 