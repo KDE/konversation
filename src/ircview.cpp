KDE/konversation

commit 1ba1e143fe42c9df29c9401e5c40106d20e7e27d

Rewrite the remember/marker line system completely to be more useful: * Distinguish between manually inserted marker lines and marker lines   inserted automatically when chat windows are hidden, fixing the utter   chaos in conjunction with the "show in all chat windows" preference. * Make the automatic marker line "sliding", i.e. remove the last auto-   matic line when inserting a new one. BUG:136917 * Only insert the automatic marker line when there's actually new text   being appended to a chat window. BUG:137535 * Add an action to clear all marker lines in a chat window. * Improve marker lines-related terminology and preferences. * Make the (marker line-related and other) identity default settings   consistent between the initial identity and additional newly created   identities.
svn path=/branches/extragear/kde3/network/konversation/; revision=733584

src/ircview.cpp
@@ -10,7 +10,7 @@
 /*
   Copyright (C) 2002 Dario Abatianni <eisfuchs@tigress.com>
   Copyright (C) 2005-2007 Peter Simonsson <psn@linux.se>
-  Copyright (C) 2006 Eike Hein <hein@kde.org>
+  Copyright (C) 2006-2007 Eike Hein <hein@kde.org>
 */
 
 #include "ircview.h"
@@ -78,7 +78,9 @@ IRCView::IRCView(QWidget* parent, Server* newServer) : KTextBrowser(parent)
     m_chatWin = 0;
     m_findParagraph=0;
     m_findIndex=0;
-    m_lastInsertionWasLine = false;
+
+    m_rememberLineParagraph = -1;
+    m_rememberLineDirtyBit = false;
 
     setAutoFormatting(QTextEdit::AutoNone);
     setUndoRedoEnabled(0);
@@ -194,6 +196,7 @@ void IRCView::clear()
 {
     m_buffer = QString();
     KTextBrowser::setText("");
+    wipeLineParagraphs();
 }
 
 void IRCView::highlightedSlot(const QString& _link)
@@ -705,22 +708,115 @@ void IRCView::append(const QString& nick,const QString& message)
     emit textToLog(QString("<%1>\t%2").arg(nick).arg(message));
 
     doAppend(line);
-
-    m_lastInsertionWasLine = false;
 }
 
-void IRCView::appendLine()
+void IRCView::insertRememberLine()
 {
-    if (m_lastInsertionWasLine) return;
+    m_rememberLineDirtyBit = true;
+}
 
-    QColor channelColor=Preferences::color(Preferences::ChannelMessage);
+void IRCView::cancelRememberLine()
+{
+    m_rememberLineDirtyBit = false;
+}
+
+void IRCView::appendRememberLine()
+{
+    m_rememberLineDirtyBit = false;
+
+    if (m_rememberLineParagraph == paragraphs() - 1)
+        return;
+
+    if (m_rememberLineParagraph > -1) removeParagraph(m_rememberLineParagraph);
+
+    repaintChanged();
+
+    appendLine(Preferences::color(Preferences::CommandMessage).name());
+
+    m_rememberLineParagraph = paragraphs() - 1;
+}
+
+void IRCView::insertMarkerLine()
+{
+    qHeapSort(m_markerLineParagraphs);
+
+    if (m_markerLineParagraphs.last() == paragraphs() - 1)
+        return;
+
+    bool rememberLineDirtyBit = m_rememberLineDirtyBit;
+    m_rememberLineDirtyBit = false;
+
+    appendLine(Preferences::color(Preferences::ActionMessage).name());
+
+    m_rememberLineDirtyBit = rememberLineDirtyBit;
+
+    m_markerLineParagraphs.append(paragraphs() - 1);
+}
+
+void IRCView::appendLine(const QString& color)
+{
+    QColor channelColor = Preferences::color(Preferences::ChannelMessage);
     m_tabNotification = Konversation::tnfNone;
 
-    QString line = "<p><font color=\"" + channelColor.name() + "\"><br><hr color=\""+Preferences::color(Preferences::CommandMessage).name()+"\" noshade></font></p>\n";
+    QString line = "<p><hr color=\""+color+"\" noshade></p>\n";
 
     doAppend(line);
+}
 
-    m_lastInsertionWasLine = true;
+void IRCView::clearLines()
+{
+    if (m_rememberLineParagraph > -1)
+        m_markerLineParagraphs.append(m_rememberLineParagraph);
+
+    if (m_markerLineParagraphs.count() > 0)
+    {
+        qHeapSort(m_markerLineParagraphs);
+
+        QValueList<int>::iterator it;
+        int removeCounter = 0;
+
+        for (it = m_markerLineParagraphs.begin(); it != m_markerLineParagraphs.end(); ++it)
+        {
+            removeParagraph((*it) - removeCounter);
+            removeCounter++;
+        }
+
+        wipeLineParagraphs();
+
+        repaintChanged();
+    }
+}
+
+bool IRCView::hasLines()
+{
+    if (m_rememberLineParagraph > -1 || m_markerLineParagraphs.count() > 0)
+        return true;
+
+    return false;
+}
+
+void IRCView::updateLineParagraphs(int numRemoved)
+{
+    if (m_rememberLineParagraph < numRemoved)
+        m_rememberLineParagraph = -1;
+    else
+        m_rememberLineParagraph -= numRemoved;
+
+    QValueList<int>::iterator it;
+
+    for (it = m_markerLineParagraphs.begin(); it != m_markerLineParagraphs.end(); ++it)
+    {
+        if ((*it) < numRemoved)
+            m_markerLineParagraphs.remove((*it));
+        else
+            (*it) -= numRemoved;
+    }
+}
+
+void IRCView::wipeLineParagraphs()
+{
+    m_markerLineParagraphs.clear();
+    m_rememberLineParagraph = -1;
 }
 
 void IRCView::appendRaw(const QString& message, bool suppressTimestamps, bool self)
@@ -739,8 +835,6 @@ void IRCView::appendRaw(const QString& message, bool suppressTimestamps, bool se
     }
 
     doAppend(line, self);
-
-    m_lastInsertionWasLine = false;
 }
 
 void IRCView::appendQuery(const QString& nick, const QString& message, bool inChannel)
@@ -773,8 +867,6 @@ void IRCView::appendQuery(const QString& nick, const QString& message, bool inCh
     emit textToLog(QString("<%1>\t%2").arg(nick).arg(message));
 
     doAppend(line);
-
-    m_lastInsertionWasLine = false;
 }
 
 void IRCView::appendAction(const QString& nick,const QString& message)
@@ -807,8 +899,6 @@ void IRCView::appendAction(const QString& nick,const QString& message)
     emit textToLog(QString("\t * %1 %2").arg(nick).arg(message));
 
     doAppend(line);
-
-    m_lastInsertionWasLine = false;
 }
 
 void IRCView::appendServerMessage(const QString& type, const QString& message, bool parseURL)
@@ -845,8 +935,6 @@ void IRCView::appendServerMessage(const QString& type, const QString& message, b
     emit textToLog(QString("%1\t%2").arg(type).arg(message));
 
     doAppend(line);
-
-    m_lastInsertionWasLine = false;
 }
 
 void IRCView::appendCommandMessage(const QString& type,const QString& message, bool important, bool parseURL, bool self)
@@ -887,8 +975,6 @@ void IRCView::appendCommandMessage(const QString& type,const QString& message, b
     emit textToLog(QString("%1\t%2").arg(type).arg(message));
 
     doAppend(line, self);
-
-    m_lastInsertionWasLine = false;
 }
 
 void IRCView::appendBacklogMessage(const QString& firstColumn,const QString& rawMessage)
@@ -924,8 +1010,6 @@ void IRCView::appendBacklogMessage(const QString& firstColumn,const QString& raw
 
     line = line.arg(time, nick, filter(message, backlogColor, NULL, false, false));
 
-    m_lastInsertionWasLine = false;
-
     doAppend(line);
 }
 
@@ -955,6 +1039,8 @@ void IRCView::scrollToBottom()
 
 void IRCView::doAppend(const QString& newLine, bool self)
 {
+    if (m_rememberLineDirtyBit) appendRememberLine();
+
     // Add line to buffer
     QString line(newLine);
 
@@ -989,14 +1075,16 @@ void IRCView::doAppend(const QString& newLine, bool self)
     {
         int numRemoved = paragraphs() - sbm;
 
-        if(numRemoved >0)
+        if (numRemoved > 0)
         {
             for (int index = numRemoved; index > 0; --index)
             {
                 removeParagraph(0);
             }
 
-            if(textselected)
+            updateLineParagraphs(numRemoved);
+
+            if (textselected)
             {
                 paraFrom -= numRemoved;
                 paraTo -= numRemoved;

src/chatwindow.cpp
@@ -7,7 +7,7 @@
 
 /*
   Copyright (C) 2002 Dario Abatianni <eisfuchs@tigress.com>
-  Copyright (C) 2006 Eike Hein <hein@kde.org>
+  Copyright (C) 2006-2007 Eike Hein <hein@kde.org>
 */
 
 #include "chatwindow.h"
@@ -151,12 +151,6 @@ void ChatWindow::setTextView(IRCView* newView)
     connect(textView,SIGNAL(textToLog(const QString&)), this,SLOT(logText(const QString&)));
     connect(textView,SIGNAL(setStatusBarTempText(const QString&)), this, SIGNAL(setStatusBarTempText(const QString&)));
     connect(textView,SIGNAL(clearStatusBarTempText()), this, SIGNAL(clearStatusBarTempText()));
-}
-
-void ChatWindow::insertRememberLine()
-{
-    if(!textView) return;
-    textView->appendLine();
 }
 
 void ChatWindow::appendRaw(const QString& message, bool suppressTimestamps)

src/chatwindow.h
@@ -7,7 +7,7 @@
 
 /*
   Copyright (C) 2002 Dario Abatianni <eisfuchs@tigress.com>
-  Copyright (C) 2006 Eike Hein <hein@kde.org>
+  Copyright (C) 2006-2007 Eike Hein <hein@kde.org>
 */
 
 #ifndef CHATWINDOW_H
@@ -74,7 +74,6 @@ class ChatWindow : public QVBox
         void setType(WindowType newType);
         WindowType getType();
 
-        virtual void insertRememberLine();
         virtual void append(const QString& nickname,const QString& message);
         virtual void appendRaw(const QString& message, bool suppressTimestamps=false);
         virtual void appendQuery(const QString& nickname,const QString& message, bool inChannel = false);

src/chatwindowbehaviour_preferences.ui
@@ -8,35 +8,18 @@
         <rect>
             <x>0</x>
             <y>0</y>
-            <width>452</width>
-            <height>420</height>
+            <width>456</width>
+            <height>489</height>
         </rect>
     </property>
-    <grid>
+    <vbox>
         <property name="name">
             <cstring>unnamed</cstring>
         </property>
         <property name="margin">
             <number>0</number>
         </property>
-        <spacer row="2" column="0">
-            <property name="name">
-                <cstring>spacer6</cstring>
-            </property>
-            <property name="orientation">
-                <enum>Vertical</enum>
-            </property>
-            <property name="sizeType">
-                <enum>Expanding</enum>
-            </property>
-            <property name="sizeHint">
-                <size>
-                    <width>41</width>
-                    <height>30</height>
-                </size>
-            </property>
-        </spacer>
-        <widget class="QGroupBox" row="0" column="0">
+        <widget class="QGroupBox">
             <property name="name">
                 <cstring>groupBox3</cstring>
             </property>
@@ -79,32 +62,13 @@
                 </widget>
                 <widget class="QCheckBox" row="3" column="0" rowspan="1" colspan="3">
                     <property name="name">
-                        <cstring>kcfg_AutoInsertRememberLineAfterMinimizing</cstring>
-                    </property>
-                    <property name="text">
-                        <string>Insert a &amp;remember line when chat window is hidden</string>
-                    </property>
-                    <property name="whatsThis" stdset="0">
-                        <string>Inserts a remember line into the chat window when you switch to another chat window or minimize the application.</string>
-                    </property>
-                </widget>
-                <widget class="QCheckBox" row="4" column="0" rowspan="1" colspan="3">
-                    <property name="name">
-                        <cstring>kcfg_ShowRememberLineInAllWindows</cstring>
-                    </property>
-                    <property name="text">
-                        <string>Show remember lines in all chat windows</string>
-                    </property>
-                </widget>
-                <widget class="QCheckBox" row="5" column="0" rowspan="1" colspan="3">
-                    <property name="name">
                         <cstring>kcfg_RedirectServerAndAppMsgToStatusPane</cstring>
                     </property>
                     <property name="text">
                         <string>&amp;Redirect status messages to the server status window</string>
                     </property>
                 </widget>
-                <widget class="QLabel" row="8" column="0">
+                <widget class="QLabel" row="6" column="0">
                     <property name="name">
                         <cstring>textLabel1</cstring>
                     </property>
@@ -118,7 +82,7 @@
                         <string>How many lines to keep in buffers, 0 = unlimited</string>
                     </property>
                 </widget>
-                <spacer row="7" column="0" rowspan="1" colspan="3">
+                <spacer row="5" column="0" rowspan="1" colspan="3">
                     <property name="name">
                         <cstring>spacer1_2</cstring>
                     </property>
@@ -135,7 +99,7 @@
                         </size>
                     </property>
                 </spacer>
-                <widget class="QCheckBox" row="6" column="0" rowspan="1" colspan="3">
+                <widget class="QCheckBox" row="4" column="0" rowspan="1" colspan="3">
                     <property name="name">
                         <cstring>kcfg_UseLiteralModes</cstring>
                     </property>
@@ -146,7 +110,7 @@
                         <string>Keep channel mode string as a combination of characters instead of translating them into human readable words. E.g. '*** Channel modes: no messages from outside' will become '*** Channel modes: n'</string>
                     </property>
                 </widget>
-                <widget class="QSpinBox" row="8" column="1">
+                <widget class="QSpinBox" row="6" column="1">
                     <property name="name">
                         <cstring>kcfg_ScrollbackMax</cstring>
                     </property>
@@ -163,7 +127,7 @@
                         <string>How many lines to keep in buffers, 0 = unlimited</string>
                     </property>
                 </widget>
-                <spacer row="8" column="2">
+                <spacer row="6" column="2">
                     <property name="name">
                         <cstring>spacer1</cstring>
                     </property>
@@ -182,7 +146,42 @@
                 </spacer>
             </grid>
         </widget>
-        <widget class="QGroupBox" row="1" column="0">
+        <widget class="QGroupBox">
+            <property name="name">
+                <cstring>groupBox3_2</cstring>
+            </property>
+            <property name="title">
+                <string>Marker Lines</string>
+            </property>
+            <vbox>
+                <property name="name">
+                    <cstring>unnamed</cstring>
+                </property>
+                <widget class="QCheckBox">
+                    <property name="name">
+                        <cstring>kcfg_AutomaticRememberLine</cstring>
+                    </property>
+                    <property name="text">
+                        <string>Mark the last position in a chat window when it is hidden</string>
+                    </property>
+                    <property name="accel">
+                        <string></string>
+                    </property>
+                    <property name="whatsThis" stdset="0">
+                        <string>Inserts a remember line into the chat window when you switch to another chat window or minimize the application.</string>
+                    </property>
+                </widget>
+                <widget class="QCheckBox">
+                    <property name="name">
+                        <cstring>kcfg_MarkerLineInAllViews</cstring>
+                    </property>
+                    <property name="text">
+                        <string>Show manually inserted lines in all chat windows</string>
+                    </property>
+                </widget>
+            </vbox>
+        </widget>
+        <widget class="QGroupBox">
             <property name="name">
                 <cstring>kcfg_AutoWhoContinuousEnabled</cstring>
             </property>
@@ -305,9 +304,25 @@
                 </widget>
             </grid>
         </widget>
-    </grid>
+        <spacer>
+            <property name="name">
+                <cstring>spacer6</cstring>
+            </property>
+            <property name="orientation">
+                <enum>Vertical</enum>
+            </property>
+            <property name="sizeType">
+                <enum>Expanding</enum>
+            </property>
+            <property name="sizeHint">
+                <size>
+                    <width>41</width>
+                    <height>16</height>
+                </size>
+            </property>
+        </spacer>
+    </vbox>
 </widget>
-
 <connections>
     <connection>
         <sender>kcfg_AutoWhoContinuousEnabled</sender>
@@ -315,13 +330,19 @@
         <receiver>kcfg_AutoWhoContinuousInterval</receiver>
         <slot>setEnabled(bool)</slot>
     </connection>
+    <connection>
+        <sender>kcfg_AutomaticRememberLine</sender>
+        <signal>toggled(bool)</signal>
+        <receiver>kcfg_UseLiteralModes</receiver>
+        <slot>setEnabled(bool)</slot>
+    </connection>
 </connections>
 <tabstops>
     <tabstop>kcfg_Beep</tabstop>
     <tabstop>kcfg_HideUnimportantEvents</tabstop>
     <tabstop>kcfg_DisableExpansion</tabstop>
-    <tabstop>kcfg_AutoInsertRememberLineAfterMinimizing</tabstop>
-    <tabstop>kcfg_ShowRememberLineInAllWindows</tabstop>
+    <tabstop>kcfg_AutomaticRememberLine</tabstop>
+    <tabstop>kcfg_MarkerLineInAllViews</tabstop>
     <tabstop>kcfg_RedirectServerAndAppMsgToStatusPane</tabstop>
     <tabstop>kcfg_UseLiteralModes</tabstop>
     <tabstop>kcfg_ScrollbackMax</tabstop>

src/commit.h
@@ -1,4 +1,4 @@
 // This COMMIT number is added to version string to be used as "patch level"
 #ifndef COMMIT
-#define COMMIT 3212
+#define COMMIT 3213
 #endif

src/config/konversation.kcfg
@@ -215,11 +215,6 @@
       <label></label>
       <whatsthis></whatsthis>
     </entry>
-    <entry key="ShowRememberLineInAllWindows" type="Bool">
-      <default>false</default>
-      <label></label>
-      <whatsthis></whatsthis>
-    </entry>
     <entry key="FocusNewQueries" type="Bool">
       <default>true</default>
       <label></label>
@@ -377,7 +372,12 @@
       <label></label>
       <whatsthis></whatsthis>
     </entry>
-    <entry key="AutoInsertRememberLineAfterMinimizing" type="Bool">
+    <entry key="AutomaticRememberLine" type="Bool">
+      <default>false</default>
+      <label></label>
+      <whatsthis></whatsthis>
+    </entry>
+    <entry key="MarkerLineInAllViews" type="Bool">
       <default>false</default>
       <label></label>
       <whatsthis></whatsthis>

src/config/preferences.cpp
@@ -58,8 +58,8 @@ Preferences::Preferences()
     addIdentity(mIdentity);
 
     KUser user(KUser::UseRealUserID);
-    setIdent(user.loginName());
-    setRealName(user.fullName());
+    mIdentity->setIdent(user.loginName());
+    mIdentity->setRealName(user.fullName());
 
     QStringList nickList;
     nickList.append(user.loginName());
@@ -67,12 +67,6 @@ Preferences::Preferences()
     nickList.append(user.loginName() + "__");
     mIdentity->setNicknameList(nickList);
 
-    setPartReason("Konversation terminated!");
-    setKickReason("User terminated!");
-
-    setShowAwayMessage(false);
-    setAwayMessage("/me is away: %s");
-    setUnAwayMessage("/me is back.");
     Konversation::ServerGroupSettingsPtr serverGroup = new Konversation::ServerGroupSettings;
     serverGroup->setName("Freenode");
     Konversation::ServerSettings server;

src/identity.cpp
@@ -27,12 +27,13 @@ Identity::Identity() : KShared()
 {
     m_id = s_availableId;
     s_availableId++;
-    setCodecName(Konversation::IRCCharsets::self()->encodingForLocale());
+
+    init();
 }
 
 Identity::Identity(int id) : KShared()
 {
-    if(id < 0)
+    if (id < 0)
     {
         m_id = s_availableId;
         s_availableId++;
@@ -41,6 +42,8 @@ Identity::Identity(int id) : KShared()
     {
         m_id = id;
     }
+
+    init();
 }
 
 Identity::Identity(const Identity& original) : KShared()
@@ -51,6 +54,20 @@ Identity::Identity(const Identity& original) : KShared()
 
 Identity::~Identity()
 {
+}
+
+void Identity::init()
+{
+    setCodecName(Konversation::IRCCharsets::self()->encodingForLocale());
+
+    setInsertRememberLineOnAway(false);
+
+    setPartReason("Konversation terminated!");
+    setKickReason("User terminated!");
+
+    setShowAwayMessage(false);
+    setAwayMessage("/me is away: %s");
+    setReturnMessage("/me is back.");
 }
 
 void Identity::copy(const Identity& original)

commit 3b74c4868e65d59c67ddc32ac1c040307bf496d9

* Fix a small regression with tab notifications from r733584. * If we can't get rid of the ugly margin around marker lines,   let's at least make it consistent above and below them.
svn path=/branches/extragear/kde3/network/konversation/; revision=733613

src/ircview.cpp
@@ -756,11 +756,10 @@ void IRCView::insertMarkerLine()
 void IRCView::appendLine(const QString& color)
 {
     QColor channelColor = Preferences::color(Preferences::ChannelMessage);
-    m_tabNotification = Konversation::tnfNone;
 
-    QString line = "<p><hr color=\""+color+"\" noshade></p>\n";
+    QString line = "<p><br><br><hr color=\""+color+"\" noshade></p>\n";
 
-    doAppend(line);
+    doAppend(line, true);
 }
 
 void IRCView::clearLines()