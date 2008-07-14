dd9fc4dc4c46ca3e6e54c1b2ee33ef14c4c9e99e
Eike Hein <hein@kde.org>
2008-07-14 08:56:07 +0000

    Minor changelog formatting fixes; bump numbers to post-rc1.
    svn path=/branches/extragear/kde3/network/konversation/; revision=832260

--- src/commit.h
+++ src/commit.h
@@ -1,4 +1,4 @@
 // This COMMIT number is added to version string to be used as "patch level"
 #ifndef COMMIT
-#define COMMIT 3238
+#define COMMIT 3239
 #endif
--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 #ifndef KONVI_VERSION
-#define KONVI_VERSION "1.1rc1"
+#define KONVI_VERSION "1.1rc1+"
 #endif
