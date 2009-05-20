2801d139532d64f60e0c92d5b195961dc61e6a36
Eike Hein <hein@kde.org>
2009-05-20 03:56:54 +0000

    Adjust for release.
    svn path=/trunk/extragear/network/konversation/; revision=970332

--- src/commit.h
+++ src/commit.h
@@ -1,4 +1,4 @@
 // This COMMIT number is added to version string to be used as "patch level"
 #ifndef COMMIT
-#define COMMIT 3401
+#define COMMIT 3402
 #endif
--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 #ifndef KONVI_VERSION
-#define KONVI_VERSION "1.1.75+"
+#define KONVI_VERSION "1.2-alpha1"
 #endif
