9368a17ecb47eca6a7362abc6ae2dd36ed046205
Peter Simonsson <peter.simonsson@gmail.com>
2009-05-23 14:55:42 +0000

    Rewrite the Sound class to reuse the MediaObject and AudioOutput instances and queue sound if needed.
    svn path=/trunk/extragear/network/konversation/; revision=971878

--- src/sound.cpp
+++ src/sound.cpp
@@ -1,42 +1,74 @@
-/*
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-*/
+// Copyright 2009  Peter Simonsson <peter.simonsson@gmail.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of
+// the License or (at your option) version 3 or any later version
+// accepted by the membership of KDE e.V. (or its successor approved
+// by the membership of KDE e.V.), which shall act as a proxy
+// defined in Section 14 of version 3 of the license.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-/*
-  begin:     Thu Jan 29 2004
-  copyright: (C) 2004 by Peter Simonsson
-  email:     psn@linux.se
-*/
+#include "sound.h"
 
-#include "sound.h" ////// header renamed
-
-#include <kurl.h>
+#include <KUrl>
 
 #include <Phonon/AudioOutput>
-#include <Phonon/MediaObject>
 
 
 namespace Konversation
 {
     Sound::Sound(QObject* parent, const char* name)
         : QObject(parent)
-        { setObjectName(name); }
+    {
+        setObjectName(name);
+        m_mediaObject = new Phonon::MediaObject(this);
+        m_audioOutput = new Phonon::AudioOutput(Phonon::NotificationCategory, this);
+        Phonon::createPath(m_mediaObject, m_audioOutput);
+
+        connect(m_mediaObject, SIGNAL(stateChanged(Phonon::State, Phonon::State)),
+                this, SLOT(tryPlayNext(Phonon::State, Phonon::State)));
+
+        m_played = false;
+    }
 
     Sound::~Sound()
-        {}
+    {}
 
     void Sound::play(const KUrl& url)
     {
-        // consider porting highlight settings to knotify (uwolfer)
-        Phonon::MediaObject *mediaObject = new Phonon::MediaObject(this);
-        mediaObject->setCurrentSource(Phonon::MediaSource(url));
-        Phonon::AudioOutput *audioOutput = new Phonon::AudioOutput(Phonon::NotificationCategory, this);
-        Phonon::Path path = Phonon::createPath(mediaObject, audioOutput);
-        mediaObject->play();
+        if(m_played && ((m_mediaObject->state() != Phonon::PausedState && m_mediaObject->state() != Phonon::StoppedState) || !m_playQueue.isEmpty()))
+        {
+            m_playQueue.enqueue(url);
+            return;
+        }
+
+        m_played = true;
+        playSound(url);
+    }
+
+    void Sound::tryPlayNext(Phonon::State newState, Phonon::State oldState)
+    {
+        Q_UNUSED(oldState);
+
+        if(newState == Phonon::PausedState && !m_playQueue.isEmpty())
+        {
+            playSound(m_playQueue.dequeue());
+        }
+    }
+
+    void Sound::playSound(const KUrl& url)
+    {
+        m_mediaObject->setCurrentSource(Phonon::MediaSource(url));
+        m_mediaObject->play();
     }
 }
 
-#include "./sound.moc"
+#include "sound.moc"
--- src/sound.h
+++ src/sound.h
@@ -1,23 +1,35 @@
-/*
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-*/
-
-/*
-  begin:     Thu Jan 29 2004
-  copyright: (C) 2004 by Peter Simonsson
-  email:     psn@linux.se
-*/
+// Copyright 2009  Peter Simonsson <peter.simonsson@gmail.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of
+// the License or (at your option) version 3 or any later version
+// accepted by the membership of KDE e.V. (or its successor approved
+// by the membership of KDE e.V.), which shall act as a proxy
+// defined in Section 14 of version 3 of the license.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef KONVERSATIONKONVERSATIONSOUND_H
 #define KONVERSATIONKONVERSATIONSOUND_H
 
-#include <qobject.h>
+#include <QObject>
+#include <QQueue>
 
+#include <Phonon/MediaObject>
 
 class KUrl;
+
+namespace Phonon
+{
+    class AudioOutput;
+}
 
 namespace Konversation
 {
@@ -36,6 +48,18 @@ namespace Konversation
         public slots:
             void play(const KUrl& url);
 
+        protected slots:
+            void tryPlayNext(Phonon::State newState, Phonon::State oldState);
+
+        protected:
+            void playSound(const KUrl& url);
+
+        private:
+            Phonon::MediaObject* m_mediaObject;
+            Phonon::AudioOutput* m_audioOutput;
+
+            QQueue<KUrl> m_playQueue;
+            bool m_played;
     };
 }
 #endif
