575f88176e38dee0491627aae3426c4b5e383b1a
Peter Simonsson <peter.simonsson@gmail.com>
2009-05-30 19:44:33 +0000

    Fix the order of the nicks in the completion box the the last active is first. BUG: 190567
    svn path=/trunk/extragear/network/konversation/; revision=975708

--- src/irc/channel.cpp
+++ src/irc/channel.cpp
@@ -66,11 +66,11 @@ bool nickTimestampLessThan(const Nick* nick1, const Nick* nick2)
 {
     int returnValue = nick2->getChannelNick()->timeStamp() - nick1->getChannelNick()->timeStamp();
     if( returnValue == 0) {
-        returnValue = QString::compare(nick2->getChannelNick()->loweredNickname(),
-                                       nick1->getChannelNick()->loweredNickname());
+        returnValue = QString::compare(nick1->getChannelNick()->loweredNickname(),
+                                       nick2->getChannelNick()->loweredNickname());
     }
 
-    return (returnValue > 0);
+    return (returnValue < 0);
 }
 
 bool nickLessThan(const Nick* nick1, const Nick* nick2)
