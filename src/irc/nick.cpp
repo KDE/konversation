8b017a02ecc83346dcc8a336f82ea7c0580e9d9a
Modestas Vainius <modestas@vainius.eu>
2009-07-24 13:29:54 +0000

    typos--
    svn path=/trunk/extragear/network/konversation/; revision=1001910

--- src/irc/nick.cpp
+++ src/irc/nick.cpp
@@ -147,7 +147,7 @@ void Nick::emitDataChanged()
 {
 #if (QT_VERSION < QT_VERSION_CHECK(4, 5, 0))
     m_emitDataChanged = !m_emitDataChanged;
-    int sortCol = treeWidget()->getSortIndicatorColumn();
+    int sortCol = treeWidget()->header()->sortIndicatorSection();
     setData((sortCol > 0) ? sortCol : 0, Qt::UserRole, emitDataChangedQVariants[m_emitDataChanged]);
 #else
     QTreeWidgetItem::emitDataChanged();
