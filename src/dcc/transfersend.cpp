865893d6f9406738aa266ceb4f5bef478de2d33f
Bernd Buschinski <b.buschinski@web.de>
2009-07-28 00:20:24 +0000

    - warning--
    svn path=/trunk/extragear/network/konversation/; revision=1003303

--- src/dcc/transfersend.cpp
+++ src/dcc/transfersend.cpp
@@ -436,7 +436,7 @@ namespace Konversation
             }
         }
 
-        void TransferSend::bytesWritten(qint64 bytes)
+        void TransferSend::bytesWritten(qint64 /*bytes*/)
         {
             //wait for all remaining bytes to be written
             if (m_sendSocket->bytesToWrite() <= m_bufferSize)
