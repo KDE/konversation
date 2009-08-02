KDE/konversation

commit df7c07fafa6ac4a8bc1d552d9130db79b603a70b

- wait for at least bufferSize bytes to be written in fastsendmode, otherwise sender gui gets out of sync, NOTE: I don't wait for all bytes to be written, as this could cause (minor) slowdowns
svn path=/trunk/extragear/network/konversation/; revision=1003204

src/dcc/transfersend.cpp
@@ -413,7 +413,7 @@ namespace Konversation
             stopConnectionTimer();
 
             if ( m_fastSend )
-                connect( m_sendSocket, SIGNAL( bytesWritten( qint64 ) ), this, SLOT( writeData() ) );
+                connect( m_sendSocket, SIGNAL( bytesWritten( qint64 ) ), this, SLOT( bytesWritten( qint64 ) ) );
             connect( m_sendSocket, SIGNAL( readyRead() ),  this, SLOT( getAck() ) );
 
             m_partnerIp = m_sendSocket->peerAddress().toString();
@@ -433,6 +433,15 @@ namespace Konversation
             else
             {
                 failed( getQFileErrorString( m_file.error() ) );
+            }
+        }
+
+        void TransferSend::bytesWritten(qint64 bytes)
+        {
+            //wait for all remaining bytes to be written
+            if (m_sendSocket->bytesToWrite() <= m_bufferSize)
+            {
+                writeData();
             }
         }
 

src/dcc/transfersend.h
@@ -68,6 +68,7 @@ namespace Konversation
                 // it must be invoked when m_sendSocket is ready
                 void startSending();
                 void writeData();
+                void bytesWritten(qint64 bytes);
                 void getAck();
                 void slotGotSocketError( QAbstractSocket::SocketError errorCode );
                 void slotConnectionTimeout();

commit 0c7b03a166cd79c27b022a9b43b8060d855d2480

- Fix crash when abort send, regression from r1003204
svn path=/trunk/extragear/network/konversation/; revision=1006022

src/dcc/transfersend.cpp
@@ -439,7 +439,7 @@ namespace Konversation
         void TransferSend::bytesWritten(qint64 /*bytes*/)
         {
             //wait for all remaining bytes to be written
-            if (m_sendSocket->bytesToWrite() <= m_bufferSize)
+            if (m_sendSocket && m_sendSocket->bytesToWrite() <= m_bufferSize)
             {
                 writeData();
             }