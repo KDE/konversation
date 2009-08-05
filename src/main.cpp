2357f5058c1746e36e2b2a59bab15bf68165288e
Eike Hein <hein@kde.org>
2009-08-05 14:11:24 +0000

    Add release blurb & adjust codebase for release.
    svn path=/trunk/extragear/network/konversation/; revision=1007316

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,6 @@
 #include <KCmdLineArgs>
 #include <KAboutData>
 
-#define HACKSTR(x) #x
-#define STRHACK(x) HACKSTR(x)
-
 /*
   Don't use i18n() here, use ki18n() instead!
   i18n() will only work as soon as a kapplication object was made.
@@ -33,7 +30,7 @@ int main(int argc, char* argv[])
     KAboutData aboutData("konversation",
         "",
         ki18n("Konversation"),
-        KONVI_VERSION " #" STRHACK(COMMIT),
+        KONVI_VERSION,
         ki18n("A user friendly IRC client"),
         KAboutData::License_GPL,
         ki18n("(C) 2002-2009 by the Konversation team"),
