KDE/konversation

commit 56389bed4dfe774fbc76ef22da6d9145f122588e

- Fix transfer send updates for fastsend - Fix possible crash on resume BUG: 208264
svn path=/trunk/extragear/network/konversation/; revision=1027336

src/dcc/transferrecv.cpp
@@ -375,6 +375,9 @@ namespace Konversation
             kDebug() << "[BEGIN]" << endl
                 << "size: " << QString::number( size );
 
+            KIO::TransferJob* transferJob = static_cast<KIO::TransferJob*>( job );
+            disconnect( transferJob, 0, 0, 0 );
+
             if ( Application::instance()->getDccTransferManager()->isLocalFileInWritingProcess( m_fileURL ) )
             {
                 askAndPrepareLocalKio( i18n( "<b>The file is used by another transfer.</b><br>"
@@ -387,11 +390,6 @@ namespace Konversation
 
             if ( size != 0 )
             {
-                KIO::TransferJob* transferJob = static_cast<KIO::TransferJob*>( job );
-
-                disconnect( transferJob, 0, 0, 0 );
-                transferJob->kill();
-
                 if ( Preferences::self()->dccAutoResume() )
                 {
                     prepareLocalKio( false, true, size );

src/dcc/transfersend.cpp
@@ -423,10 +423,7 @@ namespace Konversation
         {
             stopConnectionTimer();
 
-            if ( m_fastSend )
-            {
-                connect( m_sendSocket, SIGNAL( bytesWritten( qint64 ) ), this, SLOT( bytesWritten( qint64 ) ) );
-            }
+            connect( m_sendSocket, SIGNAL( bytesWritten( qint64 ) ), this, SLOT( bytesWritten( qint64 ) ) );
             connect( m_sendSocket, SIGNAL( readyRead() ),  this, SLOT( getAck() ) );
 
             m_partnerIp = m_sendSocket->peerAddress().toString();
@@ -461,12 +458,16 @@ namespace Konversation
                 }
             }
 
-//             kDebug() << "bytes written:" << bytes;
-//             kDebug() << "m_sendSocket->bytesToWrite():" << m_sendSocket->bytesToWrite() << "<= m_bufferSize:" << m_bufferSize;
-            //wait for all remaining bytes to be written
-            if (m_sendSocket && m_sendSocket->bytesToWrite() <= (qint64)m_bufferSize)
+            if (m_sendSocket)
             {
-                writeData();
+                if (m_fastSend && m_sendSocket->bytesToWrite() <= (qint64)m_bufferSize)
+                {
+                    writeData();
+                }
+                else if (!m_fastSend && bytes == 0)
+                {
+                    writeData();
+                }
             }
         }
 

commit e19a4f4391b6f9c7b8b6915912d9158244507271

- Fix "stuck at preparing" regression from r1027336, only disconnect job signals if we actually can resume
svn path=/trunk/extragear/network/konversation/; revision=1027775

src/dcc/transferrecv.cpp
@@ -376,10 +376,10 @@ namespace Konversation
                 << "size: " << QString::number( size );
 
             KIO::TransferJob* transferJob = static_cast<KIO::TransferJob*>( job );
-            disconnect( transferJob, 0, 0, 0 );
 
             if ( Application::instance()->getDccTransferManager()->isLocalFileInWritingProcess( m_fileURL ) )
             {
+                disconnect( transferJob, 0, 0, 0 );
                 askAndPrepareLocalKio( i18n( "<b>The file is used by another transfer.</b><br>"
                     "%1<br>",
                     m_fileURL.prettyUrl() ),
@@ -390,6 +390,7 @@ namespace Konversation
 
             if ( size != 0 )
             {
+                disconnect( transferJob, 0, 0, 0 );
                 if ( Preferences::self()->dccAutoResume() )
                 {
                     prepareLocalKio( false, true, size );