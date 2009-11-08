KDE/konversation

commit 284a05fb11c4a7760851f0736c87e6a2bfe30b12

Fix a bug that could cause user mode changes occurring directly af- ter joining a channel not to be reflected by the channel's nickname list.
svn path=/trunk/extragear/network/konversation/; revision=1046369

src/irc/channel.cpp
@@ -1703,8 +1703,23 @@ void Channel::updateMode(const QString& sourceNick, char mode, bool plus, const 
 
     // remember if this nick had any type of op.
     bool wasAnyOp=false;
-    if(parameterChannelNick)
+    if (parameterChannelNick)
+    {
+        // If NAMES processing is in progress, we likely have received
+        // a NAMES just prior to the MODE that caused this method to
+        // be run. If this nick is not yet in the nicklist (e.g. be-
+        // cause it's just after JOIN and the nicklist is still empty
+        // prior to the initial NAMES processing), the NAMES process-
+        // ing can set the ChannelNick's mode data to outdated infor-
+        // mation. By adding the nickname to the nicklist here if NA-
+        // MES processing is in progress, we prevent this, as the NA-
+        // MES processing code will ignore nicks already in the nick-
+        // list.
+        if (m_processingTimer->isActive())
+            addNickname(parameterChannelNick);
+
         wasAnyOp=parameterChannelNick->isAnyTypeOfOp();
+    }
 
     if(sourceNick.toLower()==m_server->loweredNickname())
         fromMe=true;

src/commit.h
@@ -1,4 +1,4 @@
 // This COMMIT number is added to version string to be used as "patch level"
 #ifndef COMMIT
-#define COMMIT 4005
+#define COMMIT 4006
 #endif

commit 0c29cbf04a0173b5a906c16470939a3d2c7a0b1f

Fix possible crash regression from r1046369.
svn path=/trunk/extragear/network/konversation/; revision=1046443

src/irc/channel.cpp
@@ -1715,7 +1715,10 @@ void Channel::updateMode(const QString& sourceNick, char mode, bool plus, const 
         // MES processing is in progress, we prevent this, as the NA-
         // MES processing code will ignore nicks already in the nick-
         // list.
-        if (m_processingTimer->isActive())
+        // We also add the nickname if the timer hasn't been instanci-
+        // ated yet, as this means addPendingNickList() has never run
+        // (yet) and thus NAMES hasn't been processed so far.
+        if (!m_processingTimer || m_processingTimer->isActive())
             addNickname(parameterChannelNick);
 
         wasAnyOp=parameterChannelNick->isAnyTypeOfOp();