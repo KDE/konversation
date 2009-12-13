be7f8146936a9e0a4c9c4aa075a3f8ff190e24e0
Travis McHenry <wordsizzle@gmail.com>
2009-12-13 00:52:44 -0700

    Create tooltip for the systray icon

--- src/viewer/trayiconknotificationitem.cpp
+++ src/viewer/trayiconknotificationitem.cpp
@@ -19,6 +19,8 @@ namespace Konversation
         m_notificationEnabled = false;
         
         updateAppearance();
+	
+	setToolTip("konversation", i18n("Konversation"), i18n("Konversation - IRC Client"));
     }
 
     TrayIcon::~TrayIcon()
