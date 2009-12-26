7e85576748d99f859d514b61569e7f3b19f3d4f8
Abdurrahman AVCI <abdurrahmanavci@gmail.com>
2009-12-26 16:57:41 +0200

    Port NicksOnline list to QTreeWidget.

--- src/irc/nicksonlineitem.cpp
+++ src/irc/nicksonlineitem.cpp
@@ -12,38 +12,34 @@
 #include "nicksonlineitem.h"
 
 
-NicksOnlineItem::NicksOnlineItem(int type, Q3ListView* parent, const QString& name, const QString& col2) :
-                 K3ListViewItem(parent, name, col2)
+NicksOnlineItem::NicksOnlineItem(int type, QTreeWidget* parent, const QString& name, const QString& col2) :
+                 QTreeWidgetItem(parent)
 {
+  setText(0, name);
+  setText(1, col2);
   m_type=type;
   m_connectionId = 0;
   m_offline = false;
 }
 
-NicksOnlineItem::NicksOnlineItem(int type, Q3ListViewItem* parent, const QString& name, const QString& col2) :
-                 K3ListViewItem(parent, name, col2)
+NicksOnlineItem::NicksOnlineItem(int type, QTreeWidgetItem* parent, const QString& name, const QString& col2) :
+                 QTreeWidgetItem(parent)
 {
+  setText(0, name);
+  setText(1, col2);
   m_type=type;
   m_connectionId = 0;
   m_offline = false;
 }
 
-/**
- * Reimplemented to make sure, "Offline" items always get sorted to the bottom of the list
- * @param i                 Pointer to the QListViewItem to compare with.
- * @param col               The column to compare
- * @param ascending         Specify sorting direction
- * @return                  -1 if this item's value is smaller than i, 0 if they are equal, 1 if it's greater
- */
-int NicksOnlineItem::compare(Q3ListViewItem* i,int col,bool ascending) const
+bool NicksOnlineItem::operator<(const QTreeWidgetItem &item) const
 {
   // if we are the Offline item, make sure we get sorted at the end of the list
-  if(m_type==OfflineItem) return ascending ? 1 : -1;
+  if(m_type==OfflineItem) return 1;
   // if we are competing with an Offline item, always lose
-  if(static_cast<NicksOnlineItem*>(i)->type()==OfflineItem) return ascending ? -1 : 1;
-
+  if(item.type()==OfflineItem) return -1;
   // otherwise compare items case-insensitively
-  return key(col,ascending).toLower().localeAwareCompare(i->key(col,ascending).toLower());
+  return text(treeWidget()->sortColumn()).toLower() < item.text(treeWidget()->sortColumn()).toLower();
 }
 
 /**
