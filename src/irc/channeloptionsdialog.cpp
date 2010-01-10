3ee0b28d28ad566546bef68133c9ce47f9847179
Abdurrahman AVCI <abdurrahmanavci@gmail.com>
2010-01-10 18:09:27 +0200

    Select newly added item after adding a ban.

--- src/irc/channeloptionsdialog.cpp
+++ src/irc/channeloptionsdialog.cpp
@@ -422,7 +422,9 @@ namespace Konversation
 
     void ChannelOptionsDialog::addBan(const QString& newban)
     {
-        new BanListViewItem(m_ui.banList, newban.section(' ', 0, 0), newban.section(' ', 1, 1).section('!', 0, 0), newban.section(' ', 2 ,2).toUInt());
+        BanListViewItem *item = new BanListViewItem(m_ui.banList, newban.section(' ', 0, 0), newban.section(' ', 1, 1).section('!', 0, 0), newban.section(' ', 2 ,2).toUInt());
+        // set item as current item
+        m_ui.banList->setCurrentItem(item);
     }
 
     void ChannelOptionsDialog::removeBan(const QString& ban)
