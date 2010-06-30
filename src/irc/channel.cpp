4f522c2cf77e1e825ab0a4338daa099683eaf02b
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2010-06-30 01:01:50 -0300

    Fix "what's this" text for nickname combo box.
    It mentioned File->Identities menu item,
    but Identities is in Settings now.
    
    Signed-off-by: eli mackenzie <argonel@gmail.com>

--- src/irc/channel.cpp
+++ src/irc/channel.cpp
@@ -225,7 +225,7 @@ Channel::Channel(QWidget* parent, QString _name) : ChatWindow(parent)
     nicknameCombobox->setSizeAdjustPolicy(KComboBox::AdjustToContents);
     KLineEdit* nicknameComboboxLineEdit = qobject_cast<KLineEdit*>(nicknameCombobox->lineEdit());
     if (nicknameComboboxLineEdit) nicknameComboboxLineEdit->setClearButtonShown(false);
-    nicknameCombobox->setWhatsThis(i18n("<qt><p>This shows your current nick, and any alternatives you have set up.  If you select or type in a different nickname, then a request will be sent to the IRC server to change your nick.  If the server allows it, the new nickname will be selected.  If you type in a new nickname, you need to press 'Enter' at the end.</p><p>You can add change the alternative nicknames from the <em>Identities</em> option in the <em>File</em> menu.</p></qt>"));
+    nicknameCombobox->setWhatsThis(i18n("<qt><p>This shows your current nick, and any alternatives you have set up.  If you select or type in a different nickname, then a request will be sent to the IRC server to change your nick.  If the server allows it, the new nickname will be selected.  If you type in a new nickname, you need to press 'Enter' at the end.</p><p>You can edit the alternative nicknames from the <em>Identities</em> option in the <em>Settings</em> menu.</p></qt>"));
 
     awayLabel = new QLabel(i18n("(away)"), commandLineBox);
     awayLabel->hide();
