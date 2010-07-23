KDE/konversation

commit ec68cfdf6084f3249e2674847fad15d25912eb6a

compile

src/irc/server.cpp
@@ -212,7 +212,7 @@ Server::~Server()
 
         QMetaObject::invokeMethod(konvApp->getConnectionManager(), "connectTo", Qt::QueuedConnection,
             Q_ARG(Konversation::ConnectionFlag, Konversation::CreateNewConnection),
-            Q_ARG(ConnectionSettings&, m_connectionSettings));
+            Q_ARG(ConnectionSettings, m_connectionSettings));
     }
 
     kDebug() << "~Server done";

commit 39b5cfcf179a55f2475e0447918561e189ac4d86

Fix runtime regression from ec68cfdf; should work with 4.6 and 4.7 now.

src/connectionmanager.cpp
@@ -127,7 +127,7 @@ void ConnectionManager::connectTo(Konversation::ConnectionFlag flag, const QList
     }
 }
 
-void ConnectionManager::connectTo(Konversation::ConnectionFlag flag, ConnectionSettings& settings)
+void ConnectionManager::connectTo(Konversation::ConnectionFlag flag, ConnectionSettings settings)
 {
     if (!settings.isValid()) return;
 

src/connectionmanager.h
@@ -49,7 +49,7 @@ class ConnectionManager : public QObject
 
         void connectTo(Konversation::ConnectionFlag flag, int serverGroupId);
         void connectTo(Konversation::ConnectionFlag flag, const QList<KUrl>& list);
-        void connectTo(Konversation::ConnectionFlag flag, ConnectionSettings& settings);
+        void connectTo(Konversation::ConnectionFlag flag, ConnectionSettings settings);
 
         void quitServers();
         void reconnectServers();

src/irc/server.cpp
@@ -205,7 +205,7 @@ Server::~Server()
 
     if (m_recreationScheduled)
     {
-        qRegisterMetaType<ConnectionSettings>("ConnectionSettings&");
+        qRegisterMetaType<ConnectionSettings>("ConnectionSettings");
         qRegisterMetaType<Konversation::ConnectionFlag>("Konversation::ConnectionFlag");
 
         Application* konvApp = static_cast<Application*>(kapp);