KDE/konversation

commit b3efd8ddbc091810468badc37247fdecebe74e6e

Fix WNO notifications getting logged as HTML.

src/irc/server.cpp
@@ -2808,8 +2808,10 @@ NickInfoPtr Server::setWatchedNickOnline(const QString& nickname)
     KABC::Addressee addressee = nickInfo->getAddressee();
     if (!addressee.isEmpty()) Konversation::Addressbook::self()->emitContactPresenceChanged(addressee.uid());
 
-    appendMessageToFrontmost(i18n("Notify"),"<a class=\"nick\" href=\"#"+nickname+"\">"+
-        i18n("%1 is online (%2).", nickname, getServerName())+"</a>", getStatusView());
+    // FIXME HACK: Until message routing and the ircview are refactored, there's no better
+    // way to pass the nickname down to the ircview than prepending it -- the entire append-
+    // MessageToFrontmost callgraph is pretty inflexible in terms of metadata payload, boo.
+    appendMessageToFrontmost(i18n("Notify"), nickname + ' ' + i18n("%1 is online (%2).", nickname, getServerName()), getStatusView());
 
     static_cast<Application*>(kapp)->notificationHandler()->nickOnline(getStatusView(), nickname);
 

src/viewer/ircview.cpp
@@ -811,7 +811,11 @@ void IRCView::appendServerMessage(const QString& type, const QString& message, b
     if(type != i18n("Notify"))
         line = line.arg(timeStamp(), type, filter(message, serverColor, 0 , true, parseURL));
     else
-        line = "<font color=\"" + serverColor + "\">"+line.arg(timeStamp(), type, message)+"</font>";
+    {
+        // See Server::setWatchedNickOnline() for the originating call site.
+        line = "<font color=\"" + serverColor + "\"><a class=\"nick\" href=\"#"+message.section(' ', 0, 0)+"\">"
+            +line.arg(timeStamp(), type, message.section(' ', 1))+"</a></font>";
+    }
 
     emit textToLog(QString("%1\t%2").arg(type).arg(message));
 

commit 78c9ac8f71ddd5b3b90a5fb562d6b7d26dc1ab40

Fix regression introduced by b3efd8d causing the nickname to be lost in WNO offline ircview notifications.

src/irc/server.cpp
@@ -2811,6 +2811,7 @@ NickInfoPtr Server::setWatchedNickOnline(const QString& nickname)
     // FIXME HACK: Until message routing and the ircview are refactored, there's no better
     // way to pass the nickname down to the ircview than prepending it -- the entire append-
     // MessageToFrontmost callgraph is pretty inflexible in terms of metadata payload, boo.
+    // The same trick is in Server::setWatchedNickOffline() for the offline notification.
     appendMessageToFrontmost(i18n("Notify"), nickname + ' ' + i18n("%1 is online (%2).", nickname, getServerName()), getStatusView());
 
     static_cast<Application*>(kapp)->notificationHandler()->nickOnline(getStatusView(), nickname);
@@ -2828,7 +2829,11 @@ void Server::setWatchedNickOffline(const QString& nickname, const NickInfoPtr ni
 
     emit watchedNickChanged(this, nickname, false);
 
-    appendMessageToFrontmost(i18n("Notify"), i18n("%1 went offline (%2).", nickname, getServerName()), getStatusView());
+    // FIXME HACK: Until message routing and the ircview are refactored, there's no better
+    // way to pass the nickname down to the ircview than prepending it -- the entire append-
+    // MessageToFrontmost callgraph is pretty inflexible in terms of metadata payload, boo.
+    // The same trick is in Server::setWatchedNickOffline() for the online notification.
+    appendMessageToFrontmost(i18n("Notify"), nickname + ' ' + i18n("%1 went online (%2).", nickname, getServerName()), getStatusView());
 
     static_cast<Application*>(kapp)->notificationHandler()->nickOffline(getStatusView(), nickname);
 