4b92c8c61b0a3a952d0026ea7735c64e02659680
Eike Hein <hein@kde.org>
2010-08-03 02:01:24 +0200

    s/Ok Computer/Send Computer/; <Radiohead> *sad face*

--- src/viewer/pasteeditor.cpp
+++ src/viewer/pasteeditor.cpp
@@ -2,7 +2,7 @@
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of 
+// published by the Free Software Foundation; either version 2 of
 // the License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
@@ -28,6 +28,8 @@ PasteEditor::PasteEditor(QWidget* parent)
 {
     setCaption(i18n("Edit Multiline Paste"));
     setModal(true);
+
+    setButtonText(KDialog::Ok, i18n("&Send"));
 
     QWidget* widget = new QWidget(this);
     setupUi(widget);
@@ -92,7 +94,7 @@ void PasteEditor::removeNewlines()
 
     cursor.endEditBlock();
 }
-       
+
 void PasteEditor::setText(const QString& text)
 {
     m_textEditor->setPlainText(text);
