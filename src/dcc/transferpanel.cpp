KDE/konversation

commit d2c114f67f6db1f0eae7edecdaf0486ca317970a

Use KFileMetaDataWidget to show FileInformation for >=kde-4.5

src/dcc/transferpanel.cpp
@@ -555,6 +555,10 @@ namespace Konversation
         {
             if (transfer->getType() == Transfer::Send || transfer->getStatus() == Transfer::Done)
             {
+#if KDE_IS_VERSION(4, 5, 0)
+                FileMetaDataDialog fileDialog(transfer->getFileURL(), this);
+                fileDialog.exec();
+#else
                 QStringList infoList;
 
                 QString path = transfer->getFileURL().path();
@@ -583,26 +587,18 @@ namespace Konversation
                     // display information list if any available
                     if(infoList.count())
                     {
-                        #ifdef USE_INFOLIST
-                        KMessageBox::informationList(
-                            listView(),
-                            i18n("Available information for file %1:", path),
-                            infoList,
-                            i18n("File Information")
-                            );
-                        #else
                         KMessageBox::information(
                             getTransferView(),
                             "<qt>"+infoList.join("<br>")+"</qt>",
                             i18n("File Information")
                             );
-                        #endif
                     }
                 }
                 else
                 {
                     KMessageBox::sorry(getTransferView(), i18n("No detailed information for this file found."), i18n("File Information"));
                 }
+#endif
             }
         }
 

src/dcc/transferpanel.h
@@ -23,6 +23,15 @@
 #include <QModelIndex>
 #include <QItemSelection>
 
+#include <kdeversion.h>
+
+#if KDE_IS_VERSION(4, 5, 0)
+#   include <KDialog>
+#   include <KLocalizedString>
+#   include <kfilemetadatawidget.h>
+#endif
+#include <KDebug>
+
 class QSplitter;
 class KMenu;
 class KToolBar;
@@ -34,6 +43,38 @@ namespace Konversation
         class TransferDetailedInfoPanel;
         class TransferView;
         class Transfer;
+
+#if KDE_IS_VERSION(4, 5, 0)
+        class FileMetaDataDialog : public KDialog
+        {
+            public:
+                FileMetaDataDialog(const KUrl& file, QWidget *parent = 0)
+                    : KDialog(parent)
+                {
+                    setCaption( i18nc("File Information for %1", "%1=filename", file.fileName() ) );
+                    setButtons( KDialog::Ok );
+
+                    m_fileMetaDataWidget = new KFileMetaDataWidget(this);
+
+                    KFileItemList fileList;
+                    fileList.append(KFileItem(KFileItem::Unknown, KFileItem::Unknown, file));
+                    m_fileMetaDataWidget->setItems(fileList);
+
+                    setMainWidget(m_fileMetaDataWidget);
+
+                    //known Qt problem, minium size is not set, limitation of X11 window manager
+                    setMinimumSize(QSize(sizeHint().height()*2, sizeHint().width()));
+                }
+
+                ~FileMetaDataDialog()
+                {
+                    delete m_fileMetaDataWidget;
+                }
+
+            private:
+                KFileMetaDataWidget* m_fileMetaDataWidget;
+        };
+#endif
 
         class TransferPanel : public ChatWindow
         {

commit a76fa0b5b9addc6fe6f4fa0df2db4013b48f870d

Fix krazy2 errors(spelling, crashy, explicit) introduced by d2c114f67f6db1f0eae7edecdaf0486ca317970a

src/dcc/transferpanel.cpp
@@ -556,8 +556,9 @@ namespace Konversation
             if (transfer->getType() == Transfer::Send || transfer->getStatus() == Transfer::Done)
             {
 #if KDE_IS_VERSION(4, 5, 0)
-                FileMetaDataDialog fileDialog(transfer->getFileURL(), this);
-                fileDialog.exec();
+                QPointer<FileMetaDataDialog> fileDialog = new FileMetaDataDialog(transfer->getFileURL(), this);
+                fileDialog->exec();
+                delete fileDialog;
 #else
                 QStringList infoList;
 

src/dcc/transferpanel.h
@@ -48,7 +48,7 @@ namespace Konversation
         class FileMetaDataDialog : public KDialog
         {
             public:
-                FileMetaDataDialog(const KUrl& file, QWidget *parent = 0)
+                explicit FileMetaDataDialog(const KUrl& file, QWidget *parent = 0)
                     : KDialog(parent)
                 {
                     setCaption( i18nc("File Information for %1", "%1=filename", file.fileName() ) );
@@ -62,7 +62,7 @@ namespace Konversation
 
                     setMainWidget(m_fileMetaDataWidget);
 
-                    //known Qt problem, minium size is not set, limitation of X11 window manager
+                    //known Qt problem, minimum size is not set, limitation of X11 window manager
                     setMinimumSize(QSize(sizeHint().height()*2, sizeHint().width()));
                 }
 