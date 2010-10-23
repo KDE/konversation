aae8eb44609a7355c94d0686176027dfb8c77363
Eike Hein <hein@kde.org>
2010-10-23 02:08:41 +0200

    Back to 6667, seems some Freenode servers expect SASL auth on 8001.

--- src/config/preferences.cpp
+++ src/config/preferences.cpp
@@ -64,7 +64,7 @@ Preferences::Preferences()
     serverGroup->setName("Freenode");
     Konversation::ServerSettings server;
     server.setHost("chat.freenode.net");
-    server.setPort(8001);
+    server.setPort(6667);
     serverGroup->addServer(server);
     serverGroup->setIdentityId(mIdentity->id());
     Konversation::ChannelSettings channel;
