287a9ca1fc60b1ebb55be11107ed799b8b63ac8a
Montel Laurent <montel@kde.org>
2011-02-10 13:27:12 +0100

    Fix extract messages

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/konversation.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/konversation.pot
 rm -f rc.cpp
