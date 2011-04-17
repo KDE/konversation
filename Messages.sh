4e45f0cfae22a985fdc185549af12321fb339462
Eike Hein <hein@kde.org>
2011-04-17 02:25:28 +0200

    Extract strings from the cmd script.
    The media script will get i18n support once we've seen that this whole
    thing works in practice.

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/konversation.pot
+$XGETTEXT --language=Python --join data/scripts/cmd -o $podir/konversation.pot
 rm -f rc.cpp
