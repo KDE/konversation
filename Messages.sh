8c8a86958b0c8e583d98be8d05a85263dcaebb63
Eike Hein <hein@kde.org>
2011-04-18 20:31:57 +0200

    Basic i18n support for the media script.
    I've decided not to i18n-ify the channel announce, since it's
    technically all user-configurable strings - plus it's hell not
    to make it hard on translators. So, the various info and error
    messages only for now.
    
    Plus the list of available players in the help blurb is now
    sorted alphabetically.

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,5 @@
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/konversation.pot
 $XGETTEXT --language=Python --join data/scripts/cmd -o $podir/konversation.pot
+$XGETTEXT --language=Python --join data/scripts/media -o $podir/konversation.pot
 rm -f rc.cpp
