KDE/konversation

commit e8a7b459f2d067b8d06caea37c95cc6add32a1a8

Make it compile with KDE_NO_DEPRECATED
- don't link to libkutils
- port identitydialog from KEditListBox to QGroupBox+KEditListWidget
- port from KIO::PasswordDialog to KPasswordDialog
- use QTabWidget::setMovable(true)
   -> this changes behavior, tabs are moved with LMB instead of MMB.
    On the other hand, it includes a nice animation :)

src/identitydialog.cpp
@@ -84,7 +84,7 @@ namespace Konversation
 
         m_realNameEdit->setText(m_currentIdentity->getRealName());
         m_nicknameLBox->clear();
-        m_nicknameLBox->insertStringList(m_currentIdentity->getNicknameList(), 0);//0 is workaround for kdelibs < 4.2.2 bug
+        m_nicknameLBox->insertStringList(m_currentIdentity->getNicknameList());
         m_botEdit->setText(m_currentIdentity->getBot());
         m_passwordEdit->setText(m_currentIdentity->getPassword());
 
@@ -125,13 +125,7 @@ namespace Konversation
         }
 
         m_currentIdentity->setRealName(m_realNameEdit->text());
-        QStringList nicks;
-
-        for(int i = 0; i < m_nicknameLBox->count(); ++i)
-        {
-            nicks.append(m_nicknameLBox->text(i));
-        }
-
+        const QStringList nicks = m_nicknameLBox->items();
         m_currentIdentity->setNicknameList(nicks);
         m_currentIdentity->setBot(m_botEdit->text());
         m_currentIdentity->setPassword(m_passwordEdit->text());

src/identitydialog.ui
@@ -201,15 +201,24 @@
         </layout>
        </item>
        <item>
-        <widget class="KEditListBox" name="m_nicknameLBox">
-         <property name="whatsThis">
-          <string>This is your list of nicknames. A nickname is the name that other users will know you by. You may use any name you desire. The first character must be a letter.
-
-Since nicknames must be unique across an entire IRC network, your desired name may be rejected by the server because someone else is already using that nickname. Enter alternate nicknames for yourself. If your first choice is rejected by the server, Konversation will try the alternate nicknames.</string>
-         </property>
+        <widget class="QGroupBox" name="groupBox">
          <property name="title">
           <string>Nickname</string>
          </property>
+         <layout class="QVBoxLayout" name="verticalLayout_3">
+          <property name="margin">
+           <number>0</number>
+          </property>
+          <item>
+           <widget class="KEditListWidget" name="m_nicknameLBox">
+            <property name="whatsThis">
+             <string>This is your list of nicknames. A nickname is the name that other users will know you by. You may use any name you desire. The first character must be a letter.
+
+Since nicknames must be unique across an entire IRC network, your desired name may be rejected by the server because someone else is already using that nickname. Enter alternate nicknames for yourself. If your first choice is rejected by the server, Konversation will try the alternate nicknames.</string>
+            </property>
+           </widget>
+          </item>
+         </layout>
         </widget>
        </item>
        <item>
@@ -604,19 +613,14 @@ Since nicknames must be unique across an entire IRC network, your desired name m
  </widget>
  <customwidgets>
   <customwidget>
-   <class>KEditListBox</class>
-   <extends>QGroupBox</extends>
-   <header>keditlistbox.h</header>
+   <class>KEditListWidget</class>
+   <extends>QWidget</extends>
+   <header>keditlistwidget.h</header>
   </customwidget>
   <customwidget>
-   <class>KIntSpinBox</class>
-   <extends>QSpinBox</extends>
-   <header>knuminput.h</header>
-  </customwidget>
-  <customwidget>
-   <class>KPushButton</class>
-   <extends>QPushButton</extends>
-   <header>kpushbutton.h</header>
+   <class>KComboBox</class>
+   <extends>QComboBox</extends>
+   <header>kcombobox.h</header>
   </customwidget>
   <customwidget>
    <class>KLineEdit</class>
@@ -624,9 +628,14 @@ Since nicknames must be unique across an entire IRC network, your desired name m
    <header>klineedit.h</header>
   </customwidget>
   <customwidget>
-   <class>KComboBox</class>
-   <extends>QComboBox</extends>
-   <header>kcombobox.h</header>
+   <class>KPushButton</class>
+   <extends>QPushButton</extends>
+   <header>kpushbutton.h</header>
+  </customwidget>
+  <customwidget>
+   <class>KIntSpinBox</class>
+   <extends>QSpinBox</extends>
+   <header>knuminput.h</header>
   </customwidget>
   <customwidget>
    <class>KTabWidget</class>

src/CMakeLists.txt
@@ -224,7 +224,8 @@ kde4_add_app_icon(konversation_SRCS "${CMAKE_CURRENT_SOURCE_DIR}/../data/images/
 
 kde4_add_executable(konversation ${konversation_SRCS})
 
-target_link_libraries(konversation ${KDE4_KIO_LIBS} ${KDE4_KUTILS_LIBS} ${KDE4_KPARTS_LIBS}
+target_link_libraries(konversation ${KDE4_KIO_LIBS} ${KDE4_KPARTS_LIBS} ${KDE4_KIDLETIME_LIBS}
+                            ${KDE4_KEMOTICONS_LIBS}
                             ${KDE4_KDE3SUPPORT_LIBS} ${KDE4_KABC_LIBS}
                             ${KDE4_KNOTIFYCONFIG_LIBRARY} ${PHONON_LIBRARY} ${KDE4_SOLID_LIBS})
 

src/irc/outputfilter.cpp
@@ -38,7 +38,7 @@
 #include <QByteArray>
 #include <QTextStream>
 
-#include <KIO/PasswordDialog>
+#include <KPasswordDialog>
 #include <KMessageBox>
 #include <KAboutData>
 
@@ -1322,21 +1322,14 @@ namespace Konversation
         if (input.parameter.isEmpty() || parameterList.count() == 1)
         {
             QString nick((parameterList.count() == 1) ? parameterList[0] : input.myNick);
-            QString password;
-            bool keep = false;
 
-            int ret = KIO::PasswordDialog::getNameAndPassword
-                (
-                nick,
-                password,
-                &keep,
-                i18n("Enter username and password for IRC operator privileges:"),
-                false,
-                i18n("IRC Operator Password")
-                );
-
-            if (ret == KIO::PasswordDialog::Accepted)
-                result.toServer = "OPER " + nick + ' ' + password;
+            KPasswordDialog dialog(0, KPasswordDialog::ShowUsernameLine);
+            dialog.setPrompt(i18n("Enter username and password for IRC operator privileges:"));
+            dialog.setUsername(nick);
+            dialog.setCaption(i18n("IRC Operator Password"));
+            if (dialog.exec()) {
+                result.toServer = "OPER " + dialog.username() + ' ' + dialog.password();
+            }
         }
         else
             result.toServer = "OPER " + input.parameter;

src/viewer/viewcontainer.cpp
@@ -176,8 +176,7 @@ void ViewContainer::setupTabWidget()
     m_queueTuner = new QueueTuner(m_vbox, this);
     m_queueTuner->hide();
 
-    m_tabWidget->setTabReorderingEnabled(true);
-    m_tabWidget->setTabCloseActivatePrevious(true);
+    m_tabWidget->setMovable(true);
     m_tabWidget->tabBar()->setSelectionBehaviorOnRemove(QTabBar::SelectPreviousTab);
 
     m_vbox->hide();    //m_tabWidget->hide();

commit d151bbed39ec417ad84a9d7c25b2ffa3731c9b59

Fix build on kdelibs <4.6.
Fixes regression introduced by e8a7b459.
CCMAIL:faure@kde.org

Thanks to DNS777 for reporting it.

src/identitydialog.cpp
@@ -14,6 +14,12 @@
 #include "awaymanager.h"
 #include "irccharsets.h"
 
+#if KDE_IS_VERSION(4, 6, 0)
+#include <KEditListWidget>
+#else
+#include <KEditListBox>
+#endif
+
 #include <KDialog>
 #include <KMessageBox>
 #include <KInputDialog>
@@ -33,6 +39,24 @@ namespace Konversation
         QWidget* w = new QWidget(this);
         setupUi(w);
         setMainWidget(w);
+
+#if KDE_IS_VERSION(4, 6, 0)
+        QGroupBox* nickGroupBox = new QGroupBox(i18n("Nickname"));
+        verticalLayout->insertWidget(1, nickGroupBox);
+        QVBoxLayout* nickGroupBoxLayout = new QVBoxLayout(nickGroupBox);
+        nickGroupBoxLayout->setContentsMargins(0, 0, 0, 0);
+        m_nicknameLBox = new KEditListWidget(nickGroupBox);
+        nickGroupBoxLayout->addWidget(m_nicknameLBox);
+#else
+        m_nicknameLBox = new KEditListBox(i18n("Nickname"), generalWidget);
+        verticalLayout->insertWidget(1, m_nicknameLBox);
+#endif
+        m_nicknameLBox->setWhatsThis(i18n("This is your list of nicknames. A nickname is the name that other users will "
+                                          "know you by. You may use any name you desire. The first character must be a letter.\n\n"
+                                          "Since nicknames must be unique across an entire IRC network, your desired name may be "
+                                          "rejected by the server because someone else is already using that nickname. Enter "
+                                          "alternate nicknames for yourself. If your first choice is rejected by the server, "
+                                          "Konversation will try the alternate nicknames."));
 
         newBtn->setIcon(KIcon("list-add"));
         connect(newBtn, SIGNAL(clicked()), this, SLOT(newIdentity()));

src/identitydialog.h
@@ -16,6 +16,13 @@
 #include "ui_identitydialog.h"
 
 #include <KDialog>
+#include <kdeversion.h>
+
+#if KDE_IS_VERSION(4, 6, 0)
+class KEditListWidget;
+#else
+class KEditListBox;
+#endif
 
 namespace Konversation
 {
@@ -29,13 +36,13 @@ namespace Konversation
             void setCurrentIdentity(int index);
             IdentityPtr setCurrentIdentity(IdentityPtr identity);
             IdentityPtr currentIdentity() const;
-            
+
         public slots:
             virtual void accept();
 
         signals:
             void identitiesChanged();
-            
+
         protected:
             bool checkCurrentIdentity();
 
@@ -51,6 +58,11 @@ namespace Konversation
         private:
             IdentityList m_identityList;
             IdentityPtr m_currentIdentity;
+#if KDE_IS_VERSION(4, 6, 0)
+            KEditListWidget* m_nicknameLBox;
+#else
+            KEditListBox* m_nicknameLBox;
+#endif
     };
 
 }

src/identitydialog.ui
@@ -201,27 +201,6 @@
         </layout>
        </item>
        <item>
-        <widget class="QGroupBox" name="groupBox">
-         <property name="title">
-          <string>Nickname</string>
-         </property>
-         <layout class="QVBoxLayout" name="verticalLayout_3">
-          <property name="margin">
-           <number>0</number>
-          </property>
-          <item>
-           <widget class="KEditListWidget" name="m_nicknameLBox">
-            <property name="whatsThis">
-             <string>This is your list of nicknames. A nickname is the name that other users will know you by. You may use any name you desire. The first character must be a letter.
-
-Since nicknames must be unique across an entire IRC network, your desired name may be rejected by the server because someone else is already using that nickname. Enter alternate nicknames for yourself. If your first choice is rejected by the server, Konversation will try the alternate nicknames.</string>
-            </property>
-           </widget>
-          </item>
-         </layout>
-        </widget>
-       </item>
-       <item>
         <widget class="QGroupBox" name="autoIdentifyGroup">
          <property name="title">
           <string>Auto Identify</string>
@@ -612,11 +591,6 @@ Since nicknames must be unique across an entire IRC network, your desired name m
   </layout>
  </widget>
  <customwidgets>
-  <customwidget>
-   <class>KEditListWidget</class>
-   <extends>QWidget</extends>
-   <header>keditlistwidget.h</header>
-  </customwidget>
   <customwidget>
    <class>KComboBox</class>
    <extends>QComboBox</extends>