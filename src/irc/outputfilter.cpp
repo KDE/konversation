fc6e92872f5a1772ac400eb989f0bf8a7e8892b9
Bernd Buschinski <b.buschinski@web.de>
2011-10-15 16:02:52 +0200

    cppcheck: Variable 'time_t' hides typedef with same name

--- src/irc/outputfilter.cpp
+++ src/irc/outputfilter.cpp
@@ -838,8 +838,8 @@ namespace Konversation
 
         if (request == "PING")
         {
-            unsigned int time_t = QDateTime::currentDateTime().toTime_t();
-            result.toServer = QString("PRIVMSG %1 :\x01PING %2\x01").arg(recipient).arg(time_t);
+            unsigned int timeT = QDateTime::currentDateTime().toTime_t();
+            result.toServer = QString("PRIVMSG %1 :\x01PING %2\x01").arg(recipient).arg(timeT);
             result.output = i18n("Sending CTCP-%1 request to %2.", QString::fromLatin1("PING"), recipient);
         }
         else
