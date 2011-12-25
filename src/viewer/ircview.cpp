e1effa8da2fcbf2bd6209ef3ece8a99afda69df1
Bernd Buschinski <b.buschinski@googlemail.com>
2011-12-25 19:48:04 +0100

    fix "warning: Value stored to 'xyz' is never read" found by clang3.0

--- src/viewer/ircview.cpp
+++ src/viewer/ircview.cpp
@@ -1179,8 +1179,6 @@ QString IRCView::ircTextToHtml(const QString& text, bool parseURL, const QString
     int offset;
     for (int pos = 0; pos < htmlText.length(); ++pos)
     {
-        offset = 0;
-
         //check for next relevant url or channel link to insert
         if (parseURL && pos == linkPos+linkOffset)
         {
