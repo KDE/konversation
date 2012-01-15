7242404f1990ba70a8518c904281df9892303e33
Patrick Spendrin <ps_ml@gmx.de>
2012-01-15 13:07:01 +0100

    fix for windows x64 (cherry picked from commit 5abad05a82e8ca79d90d154a5d2f5d1e3f81dfc8)

--- src/irc/outputfilter.cpp
+++ src/irc/outputfilter.cpp
@@ -1946,9 +1946,9 @@ namespace Konversation
         if (input.context && input.context->getTextView())
         {
 #if KDE_IS_VERSION(4,6,0)
-            KDebug::Block myBlock(qPrintable(QString::number((ulong)(input.context->getTextView()), 16)));
+            KDebug::Block myBlock(qPrintable(QString::number((quintptr)(input.context->getTextView()), 16)));
 #else
-            kDebug() << "view =" << qPrintable(QString::number((ulong)(input.context->getTextView()), 16));
+            kDebug() << "view =" << qPrintable(QString::number((quintptr)(input.context->getTextView()), 16));
 #endif
             kDebug() << input.context->getTextView()->document();
         }
