c7a572e209eec389f75a2b3caaf30f6554af0e66
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-10-29 13:06:36 +0100

    Drop usage of deprecated QApplication::globalStrut()

--- src/viewer/ircinput.cpp
+++ src/viewer/ircinput.cpp
@@ -81,7 +81,7 @@ QSize IRCInput::sizeHint() const
     opt.midLineWidth = 0;
     opt.state |= QStyle::State_Sunken;
 
-    QSize s = style()->sizeFromContents(QStyle::CT_LineEdit, &opt, QSize(100, h).expandedTo(QApplication::globalStrut()), this);
+    QSize s = style()->sizeFromContents(QStyle::CT_LineEdit, &opt, QSize(100, h), this);
 
     return s;
 }
